5ec167f11e895ecac9eaa7212d3ecc4598503bbd
Tom Shortall <scytale@gmail.com>
2012-03-06 13:18:04 +0000

    reorder fields in TwitterApp admin
    order now matches the order used by Twitter
    this makes it easier to avoid silly mistakes when copy/pasting values from twitter to the app

--- allauth/twitter/admin.py
+++ allauth/twitter/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 
 from models import TwitterApp, TwitterAccount
 
+class TwitterAppAdmin(admin.ModelAdmin):
+    fields = ('site', 'name', 'consumer_key', 'consumer_secret', 'request_token_url', 'authorize_url', 'access_token_url')
+
 class TwitterAccountAdmin(admin.ModelAdmin):
     raw_id_fields = ('user',)
-    
-admin.site.register(TwitterApp)
+
+admin.site.register(TwitterApp, TwitterAppAdmin)
 admin.site.register(TwitterAccount, TwitterAccountAdmin)
 

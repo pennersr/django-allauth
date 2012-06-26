f0e1683f14c7933c8db6b2723d3c959770147d93
Raymond Penners <raymond.penners@intenct.nl>
2012-06-26 23:40:29 +0200

    Added OpenID to Django admin

---
+++ allauth/socialaccount/providers/openid/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from models import OpenIDStore, OpenIDNonce
+
+
+class OpenIDStoreAdmin(admin.ModelAdmin):
+    pass
+
+class OpenIDNonceAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(OpenIDStore, OpenIDStoreAdmin)
+admin.site.register(OpenIDNonce, OpenIDNonceAdmin)

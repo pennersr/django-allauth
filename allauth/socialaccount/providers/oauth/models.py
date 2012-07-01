0c13579709d011eac3653b2793f0a02e65493749
Raymond Penners <raymond.penners@intenct.nl>
2012-07-01 22:54:23 +0200

    OAuth login forgot to handle 'next' return url, fixed

--- allauth/socialaccount/providers/oauth/models.py
+++ allauth/socialaccount/providers/oauth/models.py
@@ -1,9 +1,13 @@
 from django.core.urlresolvers import reverse
+from django.utils.http import urlencode
 
 from allauth.socialaccount.providers.base import Provider
 
 class OAuthProvider(Provider):
     def get_login_url(self, request, **kwargs):
-        return reverse(self.id + "_login")
+        url = reverse(self.id + "_login")
+        if kwargs:
+            url = url + '?' + urlencode(kwargs)
+        return url
 
 
--- allauth/socialaccount/providers/oauth2/models.py
+++ allauth/socialaccount/providers/oauth2/models.py
@@ -1,10 +1,14 @@
 from django.core.urlresolvers import reverse
+from django.utils.http import urlencode
 
 from allauth.socialaccount.providers.base import Provider
 
 class OAuth2Provider(Provider):
     def get_login_url(self, request, **kwargs):
-        return reverse(self.id + "_login")
+        url = reverse(self.id + "_login")
+        if kwargs:
+            url = url + '?' + urlencode(kwargs)
+        return url
 
     def get_scope(self):
         settings = self.get_settings()

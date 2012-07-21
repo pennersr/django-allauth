cdc03246d890bb9df93ac1fc0d75c7e86ef78e10
Rabi Alam <airlab.am@gmail.com>
2012-07-21 02:22:59 -0400

    fixing import of oauth2 urls

--- allauth/socialaccount/providers/soundcloud/urls.py
+++ allauth/socialaccount/providers/soundcloud/urls.py
@@ -1,4 +1,4 @@
-from allauth.socialaccount.providers.oauth.urls import default_urlpatterns
+from allauth.socialaccount.providers.oauth2.urls import default_urlpatterns
 from provider import SoundCloudProvider 
 
 urlpatterns = default_urlpatterns(SoundCloudProvider)

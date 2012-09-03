90210c9f9fc430be43c00e2785d2e80e3a8350b7
Raymond Penners <raymond.penners@intenct.nl>
2012-09-03 22:01:53 +0200

    Python 2.6.2 compatibility issue, fixed.

--- allauth/socialaccount/templatetags/socialaccount.py
+++ allauth/socialaccount/templatetags/socialaccount.py
@@ -13,7 +13,7 @@ class ProviderLoginURLNode(template.Node):
     def render(self, context):
         provider_id = self.provider_id_var.resolve(context)
         provider = providers.registry.by_id(provider_id)
-        query = dict([(name, var.resolve(context)) for name, var
+        query = dict([(str(name), var.resolve(context)) for name, var
                       in self.params.iteritems()])
         request = context['request']
         if not query.has_key('next'):

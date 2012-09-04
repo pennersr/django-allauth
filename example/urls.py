7fbb93d43fc4549bb3ab990cd97074e3d67f5485
terrycojones <terry@fluidinfo.com>
2012-09-03 22:58:44 -0400

    Hack to set the domain and name of the current site (once, on start-up) so that confirmation emails contain a clickable link.

--- example/urls.py
+++ example/urls.py
@@ -1,6 +1,16 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 
+
+# Set the domain and name of the example site to be local so that the
+# links we generate in confirmation emails can be clicked.
+from django.contrib.sites.models import Site
+current_site = Site.objects.get_current()
+current_site.domain = '127.0.0.1:8000'
+current_site.name = 'localhost'
+current_site.save()
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',

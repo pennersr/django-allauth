a2117ef9f7f357c751527011ca222eb5f514f535
Raymond Penners <raymond.penners@intenct.nl>
2012-10-10 23:21:22 +0200

    Twitter login broken, fixed

--- allauth/socialaccount/providers/twitter/views.py
+++ allauth/socialaccount/providers/twitter/views.py
@@ -14,7 +14,7 @@ class TwitterAPI(OAuth):
     """
     Verifying twitter credentials
     """
-    url = 'https://api.twitter.com/account/verify_credentials.json'
+    url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
 
     def get_user_info(self):
         user = simplejson.loads(self.query(self.url))

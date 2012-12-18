6cff5b5dc947467d3016e719794eabc6ea28d826
Kal Sze <swordangel@gmail.com>
2012-12-19 00:52:46 +0800

    Added initial support for StackExchange OAuth 2.0

---
+++ allauth/socialaccount/providers/stackexchange/provider.py
@@ -0,0 +1,35 @@
+from allauth.socialaccount import providers
+from allauth.socialaccount.providers.base import ProviderAccount
+from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
+from allauth.socialaccount.models import SocialToken
+
+class StackExchangeAccount(ProviderAccount):
+    def get_profile_url(self):
+        return self.account.extra_data.get('items')[0].get('link')
+
+    def get_avatar_url(self):
+        return self.account.extra_data.get('items')[0].get('profile_image')
+
+    def __unicode__(self):
+        dflt = super(StackExchangeAccount, self).__unicode__()
+        try:
+            return self.account.extra_data.get('items')[0].get('display_name')
+        except (KeyError, IndexError):
+            return dflt
+
+    def get_brand(self):
+        token = SocialToken.objects.get(account = self.account)
+        return dict(id=token.app.id,
+                    name=token.app.name)
+
+class StackExchangeProvider(OAuth2Provider):
+    id = 'stackexchange'
+    name = 'StackExchange'
+    package = 'hackerfiles.socialaccount.providers.stackexchange'
+    account_class = StackExchangeAccount
+
+    def get_default_scope(self):
+        scope = ['read_inbox', 'write_access', 'private_info']
+        return scope
+
+providers.registry.register(StackExchangeProvider)
---
+++ allauth/socialaccount/providers/stackexchange/views.py
@@ -0,0 +1,75 @@
+from allauth.socialaccount.providers.oauth2.views import (OAuth2Adapter,
+                                                          OAuth2LoginView,
+                                                          OAuth2CallbackView)
+
+from allauth.socialaccount import requests
+from allauth.socialaccount.models import SocialLogin, SocialAccount
+from allauth.utils import get_user_model
+
+from django.conf import settings
+
+from provider import StackExchangeProvider
+
+User = get_user_model()
+
+class StackExchangeOAuth2Adapter(OAuth2Adapter):
+    provider_id = StackExchangeProvider.id
+    access_token_url = 'https://stackexchange.com/oauth/access_token'
+    authorize_url = 'https://stackexchange.com/oauth/'
+    profile_url = 'https://api.stackexchange.com/2.1/me'
+
+    def complete_login(self, request, app, token):
+        resp = requests.get(self.profile_url,
+                            {
+                                'access_token': token.token,
+                                'key': settings.STACKEXCHANGE_KEYS[int(app.key)][0],
+                                'site': settings.STACKEXCHANGE_KEYS[int(app.key)][1]
+                            })
+        extra_data = resp.json
+        # extra_data is something of the form:
+        #
+        # {
+        #   "items": [
+        #     {
+        #       "user_id": 654321,
+        #       "user_type": "registered",
+        #       "creation_date": 1234567890,
+        #       "display_name": "Alice",
+        #       "profile_image": "http://www.my-site.com/my-handsome-face.jpg",
+        #       "reputation": 44,
+        #       "reputation_change_day": 0,
+        #       "reputation_change_week": 0,
+        #       "reputation_change_month": 0,
+        #       "reputation_change_quarter": 15,
+        #       "reputation_change_year": 31,
+        #       "age": 30,
+        #       "last_access_date": 1355724123,
+        #       "last_modified_date": 1332302654,
+        #       "is_employee": false,
+        #       "link": "http://stackoverflow.com/users/654321/alice",
+        #       "website_url": "http://twitter.com/alice",
+        #       "location": "Japan",
+        #       "account_id": 123456,
+        #       "badge_counts": {
+        #         "gold": 0,
+        #         "silver": 0,
+        #         "bronze": 6
+        #       },
+        #       "accept_rate": 100
+        #     }
+        #   ],
+        #   "quota_remaining": 9997,
+        #   "quota_max": 10000,
+        #   "has_more": false
+        # }
+        uid = str(extra_data['items'][0]['user_id'])
+        user = User(username=extra_data['items'][0]['display_name'])
+        account = SocialAccount(extra_data=extra_data,
+                                uid=uid,
+                                provider=self.provider_id,
+                                user=user)
+        return SocialLogin(account)
+
+oauth2_login = OAuth2LoginView.adapter_view(StackExchangeOAuth2Adapter)
+oauth2_callback = OAuth2CallbackView.adapter_view(StackExchangeOAuth2Adapter)
+

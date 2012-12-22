eb0c49fd0dab6cf78043abd4e93e4418b541c0c6
Raymond Penners <raymond.penners@intenct.nl>
2012-12-22 21:35:29 +0100

    Added mocked tests for github, facebook, soundcloud

--- allauth/socialaccount/tests.py
+++ allauth/socialaccount/tests.py
@@ -15,6 +15,9 @@ from models import SocialApp
 
 
 mocked_oauth_responses = {
+    'github': MockedResponse(200, """
+{"type":"User","organizations_url":"https://api.github.com/users/pennersr/orgs","gists_url":"https://api.github.com/users/pennersr/gists{/gist_id}","received_events_url":"https://api.github.com/users/pennersr/received_events","gravatar_id":"8639768262b8484f6a3380f8db2efa5b","followers":16,"blog":"http://www.intenct.info","avatar_url":"https://secure.gravatar.com/avatar/8639768262b8484f6a3380f8db2efa5b?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png","login":"pennersr","created_at":"2010-02-10T12:50:51Z","company":"IntenCT","subscriptions_url":"https://api.github.com/users/pennersr/subscriptions","public_repos":14,"hireable":false,"url":"https://api.github.com/users/pennersr","public_gists":0,"starred_url":"https://api.github.com/users/pennersr/starred{/owner}{/repo}","html_url":"https://github.com/pennersr","location":"The Netherlands","bio":null,"name":"Raymond Penners","repos_url":"https://api.github.com/users/pennersr/repos","followers_url":"https://api.github.com/users/pennersr/followers","id":201022,"following":0,"email":"raymond.penners@intenct.nl","events_url":"https://api.github.com/users/pennersr/events{/privacy}","following_url":"https://api.github.com/users/pennersr/following"}
+"""),
     'google': MockedResponse(200, """
 {"family_name": "Penners", "name": "Raymond Penners", 
                "picture": "https://lh5.googleusercontent.com/-GOFYGBVOdBQ/AAAAAAAAAAI/AAAAAAAAAGM/WzRfPkv4xbo/photo.jpg", 
@@ -23,6 +26,32 @@ mocked_oauth_responses = {
                "link": "https://plus.google.com/108204268033311374519", 
                "given_name": "Raymond", "id": "108204268033311374519", 
                 "verified_email": true}
+"""),
+    'facebook': MockedResponse(200, """
+{
+   "id": "630595557",
+   "name": "Raymond Penners",
+   "first_name": "Raymond",
+   "last_name": "Penners",
+   "link": "https://www.facebook.com/raymond.penners",
+   "username": "raymond.penners",
+   "birthday": "07/17/1973",
+   "work": [
+      {
+         "employer": {
+            "id": "204953799537777",
+            "name": "IntenCT"
+         }
+      }
+   ],
+   "timezone": 1,
+   "locale": "nl_NL",
+   "verified": true,
+   "updated_time": "2012-11-30T20:40:33+0000"
+}
+"""),
+    'soundcloud': MockedResponse(200, """
+{"website": null, "myspace_name": null, "public_favorites_count": 0, "followings_count": 1, "full_name": "", "id": 22341947, "city": null, "track_count": 0, "playlist_count": 0, "discogs_name": null, "private_tracks_count": 0, "followers_count": 0, "online": true, "username": "user187631676", "description": null, "kind": "user", "website_title": null, "primary_email_confirmed": false, "permalink_url": "http://soundcloud.com/user187631676", "private_playlists_count": 0, "permalink": "user187631676", "country": null, "uri": "https://api.soundcloud.com/users/22341947", "avatar_url": "https://a1.sndcdn.com/images/default_avatar_large.png?4b4189b", "plan": "Free"}
 """),
     'stackexchange': MockedResponse(200, """
 {"has_more": false, "items": [{"is_employee": false, "last_access_date": 1356200390, "display_name": "pennersr", "account_id": 291652, "badge_counts": {"bronze": 2, "silver": 2, "gold": 0}, "last_modified_date": 1356199552, "profile_image": "http://www.gravatar.com/avatar/053d648486d567d3143d6bad8df8cfeb?d=identicon&r=PG", "user_type": "registered", "creation_date": 1296223711, "reputation_change_quarter": 148, "reputation_change_year": 378, "reputation": 504, "link": "http://stackoverflow.com/users/593944/pennersr", "reputation_change_week": 0, "user_id": 593944, "reputation_change_month": 10, "reputation_change_day": 0}], "quota_max": 10000, "quota_remaining": 9999}

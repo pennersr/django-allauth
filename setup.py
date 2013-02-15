5461ecca781a396bf242a34914b5d8b0fe6f3269
Raymond Penners <raymond.penners@intenct.nl>
2013-02-15 23:00:41 +0100

    Prepping for upcoming release (closes #192)

--- setup.py
+++ setup.py
@@ -98,7 +98,7 @@ package_data = find_package_data(exclude_directories=excluded_directories)
 
 METADATA = dict(
     name='django-allauth',
-    version='0.9.0',
+    version='0.10.0',
     author='Raymond Penners',
     author_email='raymond.penners@intenct.nl',
     description='Integrated set of Django applications addressing authentication, registration, account management as well as 3rd party (social) account authentication.',

7992a7834d86052608d09255a315c6cde1953411
Robert Balfre <emailcohort@gmail.com>
2013-02-26 14:48:36 +0000

    Dropbox support init

---
+++ allauth/socialaccount/providers/dropbox/provider.py
@@ -0,0 +1,18 @@
+from allauth.socialaccount import providers
+from allauth.socialaccount.providers.base import ProviderAccount
+from allauth.socialaccount.providers.oauth.provider import OAuthProvider
+
+from allauth.socialaccount import app_settings
+
+class DropboxAccount(ProviderAccount):
+    pass
+
+
+class DropboxProvider(OAuthProvider):
+    id = 'dropbox'
+    name = 'Dropbox'
+    package = 'allauth.socialaccount.providers.dropbox'
+    account_class = DropboxAccount
+
+
+providers.registry.register(DropboxProvider)

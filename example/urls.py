5e1f876e82f687d9c43efc5122c4a86151335380
Ad_m <ad_m@dobrytata.pl>
2013-03-03 17:22:19 +0100

    The last import update

--- example/urls.py
+++ example/urls.py
@@ -1,4 +1,8 @@
-from django.conf.urls import patterns, include, url
+try:
+    from django.conf.urls import patterns, url, include
+except ImportError:
+    # for Django version less then 1.4
+    from django.conf.urls.defaults import patterns, url, include
 from django.contrib import admin
 
 admin.autodiscover()

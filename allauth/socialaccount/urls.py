6ce16bf503243f7eed11777080f4dddd5838f410
Ad_m <ad_m@dobrytata.pl>
2013-03-03 17:15:14 +0100

    Update by django.conf.urls.defaults depreaction warning

--- allauth/socialaccount/urls.py
+++ allauth/socialaccount/urls.py
@@ -1,4 +1,8 @@
-from django.conf.urls import patterns, url
+try:
+    from django.conf.urls import patterns, url
+except ImportError:
+    # for Django version less then 1.4
+    from django.conf.urls.defaults import patterns, url
 
 import views
 

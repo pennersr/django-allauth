3284f772bd9e87ca895225126310f7b594a7e9b1
Sam Solomon <sssbox@gmail.com>
2013-03-14 15:51:38 -0700

    [anonymizers] Make sure it is unique.

--- allauth/socialaccount/anonymizers.py
+++ allauth/socialaccount/anonymizers.py
@@ -4,6 +4,10 @@ from anonymizer import Anonymizer
 
 similar_datetime = lambda anon, obj, field, val: make_aware(anon.faker.datetime(field=field, val=val), utc)
 
+def unique_varchar(anon, obj, field, val):
+    field.unique = True
+    return anon.faker.varchar(anon, obj, field, val)
+
 empty_json_obj = lambda anon, obj, field, val: '{}'
 
 class SocialAppAnonymizer(Anonymizer):
@@ -28,7 +32,7 @@ class SocialAccountAnonymizer(Anonymizer):
         ('id', "SKIP"),
         ('user_id', "SKIP"),
         ('provider', "SKIP"),
-        ('uid', "varchar"),
+        ('uid', unique_varchar),
         ('last_login', similar_datetime),
         ('date_joined', similar_datetime),
         ('extra_data', empty_json_obj),

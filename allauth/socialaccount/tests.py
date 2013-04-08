5e3e2a28c46e2ee8bf786c376eb9ce9752f66117
Raymond Penners <raymond.penners@intenct.nl>
2013-04-08 21:20:25 +0200

    Test case for Vimeo

--- allauth/socialaccount/tests.py
+++ allauth/socialaccount/tests.py
@@ -18,9 +18,63 @@ from ..tests import MockedResponse, mocked_response
 from ..account import app_settings as account_settings
 from ..account.models import EmailAddress
 
-from . import providers
 from .models import SocialApp, SocialAccount, SocialLogin
 from .helpers import complete_social_login
+
+def create_oauth_tests(provider):
+
+    def get_mocked_response(self):
+        pass
+
+    def setUp(self):
+        app = SocialApp.objects.create(provider=provider.id,
+                                       name=provider.id,
+                                       key=provider.id,
+                                       secret='dummy')
+        app.sites.add(Site.objects.get_current())
+
+    @override_settings(SOCIALACCOUNT_AUTO_SIGNUP=False)
+    def test_login(self):
+        resp_mock = self.get_mocked_response()
+        if not resp_mock:
+            warnings.warn("Cannot test provider %s, no oauth mock"
+                          % self.provider.id)
+            return
+        resp = self.login(resp_mock)
+        self.assertRedirects(resp, reverse('socialaccount_signup'))
+
+    def login(self, resp_mock):
+        with mocked_response(MockedResponse(200,
+                                            'oauth_token=token&'
+                                            'oauth_token_secret=psst',
+                                            {'content-type':
+                                             'text/html'})):
+            resp = self.client.get(reverse(self.provider.id + '_login'))
+        p = urlparse(resp['location'])
+        q = parse_qs(p.query)
+        complete_url = reverse(self.provider.id+'_callback')
+        self.assertGreater(q['oauth_callback'][0]
+                           .find(complete_url), 0)
+        with mocked_response(MockedResponse(200,
+                                            'oauth_token=token&'
+                                            'oauth_token_secret=psst',
+                                            {'content-type':
+                                             'text/html'}),
+                             resp_mock):
+            resp = self.client.get(complete_url)
+        return resp
+
+
+
+
+    impl = { 'setUp': setUp,
+             'login': login,
+             'test_login': test_login,
+             'get_mocked_response': get_mocked_response }
+    class_name = 'OAuth2Tests_'+provider.id
+    Class = type(class_name, (TestCase,), impl)
+    Class.provider = provider
+    return Class
 
 def create_oauth2_tests(provider):
 
@@ -28,12 +82,11 @@ def create_oauth2_tests(provider):
         pass
 
     def setUp(self):
-        for provider in providers.registry.get_list():
-            app = SocialApp.objects.create(provider=provider.id,
-                                           name=provider.id,
-                                           key=provider.id,
-                                           secret='dummy')
-            app.sites.add(Site.objects.get_current())
+        app = SocialApp.objects.create(provider=provider.id,
+                                       name=provider.id,
+                                       key=provider.id,
+                                       secret='dummy')
+        app.sites.add(Site.objects.get_current())
 
     @override_settings(SOCIALACCOUNT_AUTO_SIGNUP=False)
     def test_login(self):

a786d277c7eaa6d17239a666c39db3abab346d75
Raymond Penners <raymond.penners@intenct.nl>
2013-11-16 10:02:23 +0100

    Login silently failed on Django 1.6 (closes #447)

--- allauth/account/utils.py
+++ allauth/account/utils.py
@@ -132,7 +132,7 @@ def perform_login(request, user, email_verification,
     # whereas I do see the downsides (having to bother the integrator
     # to set up authentication backends in settings.py
     if not hasattr(user, 'backend'):
-        user.backend = "django.contrib.auth.backends.ModelBackend"
+        user.backend = "allauth.account.auth_backends.AuthenticationBackend"
     signals.user_logged_in.send(sender=user.__class__,
                                 request=request,
                                 user=user,

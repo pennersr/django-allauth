02bf640ba1379edec3e6a1af38459f058216f0c4
Raymond Penners <raymond.penners@intenct.nl>
2013-11-19 11:14:06 +0100

    Made email rendering overridable

--- allauth/account/adapter.py
+++ allauth/account/adapter.py
@@ -50,9 +50,9 @@ class DefaultAccountAdapter(object):
             prefix = u"[{name}] ".format(name=site.name)
         return prefix + force_text(subject)
 
-    def send_mail(self, template_prefix, email, context):
+    def render_mail(self, template_prefix, email, context):
         """
-        Sends an e-mail to `email`.  `template_prefix` identifies the
+        Renders an e-mail to `email`.  `template_prefix` identifies the
         e-mail that is to be sent, e.g. "account/email/email_confirmation"
         """
         subject = render_to_string('{0}_subject.txt'.format(template_prefix),
@@ -84,6 +84,10 @@ class DefaultAccountAdapter(object):
                                settings.DEFAULT_FROM_EMAIL,
                                [email])
             msg.content_subtype = 'html'  # Main content is now text/html
+        return msg
+
+    def send_mail(self, template_prefix, email, context):
+        msg = self.render_mail(template_prefix, email, context)
         msg.send()
 
     def get_login_redirect_url(self, request):

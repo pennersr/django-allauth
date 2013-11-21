30c73e7d337621a157b99edb1e003e8b73ee68e0
Stuart Ross <stuross04@gmail.com>
2013-11-20 18:06:53 -0800

    updated linkedin view to grab json data

--- allauth/socialaccount/providers/linkedin_oauth2/views.py
+++ allauth/socialaccount/providers/linkedin_oauth2/views.py
@@ -1,9 +1,4 @@
 import requests
-from xml.etree import ElementTree
-from xml.parsers.expat import ExpatError
-
-from django.utils import six
-
 from allauth.socialaccount import providers
 from allauth.socialaccount.providers.oauth2.views import (OAuth2Adapter,
                                                           OAuth2LoginView,
@@ -26,35 +21,9 @@ class LinkedInOAuth2Adapter(OAuth2Adapter):
         fields = providers.registry \
             .by_id(LinkedInOAuth2Provider.id) \
             .get_profile_fields()
-        url = self.profile_url + ':(%s)' % ','.join(fields)
+        url = self.profile_url + ':(%s)?format=json' % ','.join(fields)
         resp = requests.get(url, params={'oauth2_access_token': token.token})
-        raw_xml = resp.text
-        if not six.PY3:
-            raw_xml = raw_xml.encode('utf8')
-        try:
-            return self.to_dict(ElementTree.fromstring(raw_xml))
-        except (ExpatError, KeyError, IndexError):
-            return None
-
-    def to_dict(self, xml):
-        """
-        Convert XML structure to dict recursively, repeated keys
-        entries are returned as in list containers.
-        """
-        children = list(xml)
-        if not children:
-            return xml.text
-        else:
-            out = {}
-            for node in list(xml):
-                if node.tag in out:
-                    if not isinstance(out[node.tag], list):
-                        out[node.tag] = [out[node.tag]]
-                    out[node.tag].append(self.to_dict(node))
-                else:
-                    out[node.tag] = self.to_dict(node)
-            return out
-
+        return resp.json()
 
 oauth2_login = OAuth2LoginView.adapter_view(LinkedInOAuth2Adapter)
 oauth2_callback = OAuth2CallbackView.adapter_view(LinkedInOAuth2Adapter)

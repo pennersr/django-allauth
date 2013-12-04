bdb0222799b318aa255aa442251c3b3c98a46cda
Bryce Nesbitt <bryce2@obviously.com>
2013-12-04 10:52:37 -0800

    Since social api tokens are often long, make the admin boxes long enough to see the entire token

--- allauth/socialaccount/admin.py
+++ allauth/socialaccount/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
+from django.db      import models
+from django.forms   import TextInput, Textarea
 
-from .models import SocialApp, SocialAccount, SocialToken
-
+from  .models  import SocialApp, SocialAccount, SocialToken
 from ..account import app_settings
 
-
 class SocialAppAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size':'100'})}
+    }
     list_display = ('name', 'provider',)
     filter_horizontal = ('sites',)
 

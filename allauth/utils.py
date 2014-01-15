7e9a363813a03b636fd6e7e32ea4063fb19d5907
Raymond Penners <raymond.penners@intenct.nl>
2014-01-15 19:37:51 +0100

    Amazon HTTPS (closes #507)

--- allauth/utils.py
+++ allauth/utils.py
@@ -179,3 +179,11 @@ def set_form_field_order(form, fields_order):
         assert isinstance(form.fields, OrderedDict)
         form.fields = OrderedDict((f, form.fields[f])
                                   for f in fields_order)
+
+
+def build_absolute_uri(request, location, protocol=None):
+    uri = request.build_absolute_uri(location)
+    if protocol:
+        uri = protocol + ':' + uri.partition(':')[2]
+    return uri
+    

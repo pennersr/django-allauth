1fbe5c29f63820a726e243aa4f4c46be03569933
replay <mauro.stettler@gmail.com>
2014-02-12 18:38:42 +0300

    move email confirmation from model into adapter

--- allauth/account/adapter.py
+++ allauth/account/adapter.py
@@ -259,6 +259,13 @@ class DefaultAccountAdapter(object):
                             status=status,
                             content_type='application/json')
 
+    def confirm_email(self, email_address):
+        """
+        Marks the email address as confirmed on the db
+        """
+        email_address.verified = True
+        email_address.set_as_primary(conditional=True)
+        email_address.save()
 
 def get_adapter():
     return import_attribute(app_settings.ADAPTER)()

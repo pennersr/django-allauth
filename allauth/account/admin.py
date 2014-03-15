8c594b076ac8deef9020c3f2af0e4820a64b82b7
Brandon Jurewicz <brandonjur@gmail.com>
2014-03-15 02:18:43 -0400

    "verify e-mail" action added to EmailAddress model admin

--- allauth/account/admin.py
+++ allauth/account/admin.py
@@ -3,10 +3,14 @@ from django.contrib import admin
 from .models import EmailConfirmation, EmailAddress
 from . import app_settings
 from ..utils import get_user_model
+from . import signals
+from .adapter import get_adapter
 
 User = get_user_model()
 
+
 class EmailAddressAdmin(admin.ModelAdmin):
+    actions = ['verify_email']
     list_display = ('email', 'user', 'primary', 'verified')
     list_filter = ('primary', 'verified')
     search_fields = ['email'] + list(map(lambda a: 'user__' + a,
@@ -15,6 +19,18 @@ class EmailAddressAdmin(admin.ModelAdmin):
                                             'first_name',
                                             'last_name'])))
     raw_id_fields = ('user',)
+
+    def verify_email(self, request, queryset):
+        """
+        A way to verify a user email if their email provider refuses to accept your email.
+        """
+        for email_address in queryset:
+            get_adapter().confirm_email(request, email_address)
+            signals.email_confirmed.send(sender=self.__class__,
+                                         request=request,
+                                         email_address=email_address)
+    verify_email.short_description = "Verify E-mail"
+
 
 class EmailConfirmationAdmin(admin.ModelAdmin):
     list_display = ('email_address', 'created', 'sent', 'key')

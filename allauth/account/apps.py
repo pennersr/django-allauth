4baa01f0b321a899471a4bc00c347875f78201df
Kamil Karwowski <kkarwowski@cyfrowypolsat.pl>
2014-05-07 15:04:23 +0200

    remove unnecessary import workaround

--- allauth/account/apps.py
+++ allauth/account/apps.py
@@ -1,9 +1,5 @@
-try:
-    from django.apps import AppConfig
-except ImportError:
-    class AppConfig:
-        pass
-
+#  require django >= 1.7
+from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
 

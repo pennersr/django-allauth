5cf9f2b781ee544c2d809ed011837dd9979f7138
Matt Jackson <email@mwjackson.net>
2014-08-21 15:33:31 +0100

    created copy of auth backeend for non rel
    email auth method query errors due to unsupported query (probably the reverse foreign key lookup)
    simplify by only looking up email on auth_user for now

--- allauth/account/auth_backends.py
+++ allauth/account/auth_backends.py
@@ -53,3 +53,48 @@ class AuthenticationBackend(ModelBackend):
                 if user.check_password(credentials["password"]):
                     return user
         return None
+
+
+class NonRelAuthenticationBackend(ModelBackend):
+
+    def authenticate(self, **credentials):
+        ret = None
+        if app_settings.AUTHENTICATION_METHOD == AuthenticationMethod.EMAIL:
+            ret = self._authenticate_by_email(**credentials)
+        elif app_settings.AUTHENTICATION_METHOD \
+                == AuthenticationMethod.USERNAME_EMAIL:
+            ret = self._authenticate_by_email(**credentials)
+            if not ret:
+                ret = self._authenticate_by_username(**credentials)
+        else:
+            ret = self._authenticate_by_username(**credentials)
+        return ret
+
+    def _authenticate_by_username(self, **credentials):
+        username_field = app_settings.USER_MODEL_USERNAME_FIELD
+        username = credentials.get('username')
+        password = credentials.get('password')
+        if not username_field or username is None or password is None:
+            return None
+        try:
+            # Username query is case insensitive
+            query = {username_field+'__iexact': username}
+            user = User.objects.get(**query)
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
+            return None
+
+    def _authenticate_by_email(self, **credentials):
+        # Even though allauth will pass along `email`, other apps may
+        # not respect this setting. For example, when using
+        # django-tastypie basic authentication, the login is always
+        # passed as `username`.  So let's place nice with other apps
+        # and use username as fallback
+        email = credentials.get('email', credentials.get('username'))
+        if email:
+            users = User.objects.filter(email__iexact=email)
+            for user in users:
+                if user.check_password(credentials["password"]):
+                    return user
+        return None

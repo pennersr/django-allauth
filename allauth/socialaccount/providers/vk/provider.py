7853b1ad19f8043f6275d5b7f1270c768c764499
Repetilo Andrey <andrij.re@gmail.com>
2014-08-28 09:20:11 +0300

    add email to vk scope

--- allauth/socialaccount/providers/vk/provider.py
+++ allauth/socialaccount/providers/vk/provider.py
@@ -1,6 +1,7 @@
 from allauth.socialaccount import providers
 from allauth.socialaccount.providers.base import ProviderAccount
 from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
+from allauth.socialaccount import app_settings
 
 
 class VKAccount(ProviderAccount):
@@ -30,14 +31,17 @@ class VKProvider(OAuth2Provider):
     account_class = VKAccount
 
     def get_default_scope(self):
-        return ['email']
+        scope = []
+        if app_settings.QUERY_EMAIL:
+            scope.append('email')
+        return scope
 
     def extract_uid(self, data):
         return str(data['uid'])
 
     def extract_common_fields(self, data):
         return dict(email=data.get('email'),
-		    last_name=data.get('last_name'),
+                    last_name=data.get('last_name'),
                     username=data.get('screen_name'),
                     first_name=data.get('first_name'))
 

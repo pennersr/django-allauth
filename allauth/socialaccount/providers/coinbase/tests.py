5c0fc3b6d935bd18226c66a170edc90a697c9f91
Josh Owen <josh@openfolio.com>
2014-09-05 15:50:24 -0400

    first stab at coinbase integration

---
+++ allauth/socialaccount/providers/coinbase/tests.py
@@ -0,0 +1,15 @@
+from allauth.socialaccount.tests import create_oauth2_tests
+from allauth.tests import MockedResponse
+from allauth.socialaccount.providers import registry
+
+from .provider import CoinbaseProvider
+
+class CoinbaseTests(create_oauth2_tests(registry.by_id(CoinbaseProvider.id))):
+    def get_mocked_response(self):
+        return MockedResponse(200, """
+        {
+            "user_id": "123456",
+            "name": "Jane Doe",
+            "email": "janedoe@coinbase.com"
+        }
+        """)

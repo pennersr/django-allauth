410e321b7c82d19cf3215d2a2097de92bc101475
Josh Owen <josh@openfolio.com>
2014-09-05 20:28:11 -0400

    use full test response

--- allauth/socialaccount/providers/coinbase/tests.py
+++ allauth/socialaccount/providers/coinbase/tests.py
@@ -7,15 +7,39 @@ from .provider import CoinbaseProvider
 class CoinbaseTests(create_oauth2_tests(registry.by_id(CoinbaseProvider.id))):
     def get_mocked_response(self):
         return MockedResponse(200, """
-        {
-            "users" : [
-                        {
-                            "user": {
-                                "id": "123456",
-                                "name": "Jane Doe",
-                                "email": "janedoe@coinbase.com"
-                            }
-                        }
-            ]
+{
+  "users": [
+    {
+      "user": {
+        "id": "512db383f8182bd24d000001",
+        "name": "User One",
+        "email": "user1@example.com",
+        "time_zone": "Pacific Time (US & Canada)",
+        "native_currency": "USD",
+        "balance": {
+          "amount": "49.76000000",
+          "currency": "BTC"
+        },
+        "merchant": {
+          "company_name": "Company Name, Inc.",
+          "logo": {
+            "small": "http://smalllogo.example",
+            "medium": "http://mediumlogo.example",
+            "url": "http://logo.example"
+          }
+        },
+        "buy_level": 1,
+        "sell_level": 1,
+        "buy_limit": {
+          "amount": "10.00000000",
+          "currency": "BTC"
+        },
+        "sell_limit": {
+          "amount": "100.00000000",
+          "currency": "BTC"
         }
+      }
+    }
+  ]
+}
         """)

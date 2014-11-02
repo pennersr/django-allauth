becc704800b39fdc86d93f8dcde20abf0088877b
Christopher Grebs <cg@webshox.org>
2014-11-02 14:19:10 +0100

    Remove the requirement for SILENCED_SYSTEM_CHECKS, this is actually quite silly but... I don't know any better way to support all Django versions 1.4-1.7

--- allauth/account/admin.py
+++ allauth/account/admin.py
@@ -11,8 +11,29 @@ class EmailAddressAdmin(admin.ModelAdmin):
     list_filter = ('primary', 'verified')
     raw_id_fields = ('user',)
 
-    @staticmethod
-    def _get_search_fields():
+    @classmethod
+    def check(cls, *args, **kwargs):
+        """Django 1.7: Silence `list` check for `search_fields.
+
+        Required to silence the check if `ModelAdmin.search_fields` is a list.
+        We generate this list dynamically, thus it's working but the check
+        framework does not evaluate the expressions and fails.
+        """
+        errors = super(EmailAddressAdmin, cls).check(*args, **kwargs)
+
+        expected_msg = "The value of 'search_fields' must be a list or tuple."
+
+        def _filter(err):
+            if err.id == 'admin.E126' and err.msg == expected_msg:
+                return
+            return err
+
+        errors = list(filter(_filter, errors))
+
+        return errors
+
+    @classmethod
+    def _get_search_fields(cls):
         return get_possible_search_fields(['email'])
 
     search_fields = allow_lazy(_get_search_fields, list)
--- allauth/socialaccount/admin.py
+++ allauth/socialaccount/admin.py
@@ -30,8 +30,29 @@ class SocialAccountAdmin(admin.ModelAdmin):
     list_display = ('user', 'uid', 'provider')
     list_filter = ('provider',)
 
-    @staticmethod
-    def _get_search_fields():
+    @classmethod
+    def check(cls, *args, **kwargs):
+        """Django 1.7: Silence `list` check for `search_fields.
+
+        Required to silence the check if `ModelAdmin.search_fields` is a list.
+        We generate this list dynamically, thus it's working but the check
+        framework does not evaluate the expressions and fails.
+        """
+        errors = super(SocialAccountAdmin, cls).check(*args, **kwargs)
+
+        expected_msg = "The value of 'search_fields' must be a list or tuple."
+
+        def _filter(err):
+            if err.id == 'admin.E126' and err.msg == expected_msg:
+                return
+            return err
+
+        errors = list(filter(_filter, errors))
+
+        return errors
+
+    @classmethod
+    def _get_search_fields(cls):
         return get_possible_search_fields(['user__emailaddress__email'])
 
     search_fields = allow_lazy(_get_search_fields, list)

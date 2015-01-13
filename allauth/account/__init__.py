d63b6c699e180bcd9b1fd2e568da3f6f79b27ee2
Wellington Cordeiro <wellington@wellingtoncordeiro.com>
2015-01-13 12:42:46 -0700

    Fixing rsplit

--- allauth/account/__init__.py
+++ allauth/account/__init__.py
@@ -7,7 +7,7 @@ from . import app_settings
 
 
 def signup_form():
-    class_module, form_module, class_name = app_settings.SIGNUP_FORM_CLASS.rsplit('.', 1)
+    class_module, form_module, class_name = app_settings.SIGNUP_FORM_CLASS.rsplit('.', 2)
     mod = import_module(form_module)
     return getattr(mod, class_name)()
 

6e6b9e0f1ecea1830e3ae8cd023ab7bc4c5e5254
Wellington Cordeiro <wellington@wellingtoncordeiro.com>
2015-01-14 11:42:09 -0700

    Debug message fix.

--- allauth/account/adapter.py
+++ allauth/account/adapter.py
@@ -181,26 +181,32 @@ class DefaultAccountAdapter(object):
         from .utils import user_username, user_email, user_field
 
         data = form.cleaned_data
-        print("USER DATA:\n" + data)
+        print("USER DATA:\n {}".format(data))
         first_name = data.get('first_name')
         last_name = data.get('last_name')
         email = data.get('email')
         username = data.get('username')
         user_email(user, email)
         user_username(user, username)
+
         if first_name:
             user_field(user, 'first_name', first_name)
+
         if last_name:
             user_field(user, 'last_name', last_name)
+
         if 'password1' in data:
             user.set_password(data["password1"])
         else:
             user.set_unusable_password()
+
         self.populate_username(request, user)
+
         if commit:
             # Ability not to commit makes it easier to derive from
             # this adapter by adding
             user.save()
+
         return user
 
     def clean_username(self, username):

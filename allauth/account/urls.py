dbde54f91ab0b935a65bb92da4df17fffd973615
Víðir Valberg Guðmundsson <valberg@orn.li>
2015-03-08 22:55:44 +0100

    Only load urls if setting is set.

--- allauth/account/urls.py
+++ allauth/account/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 from django.views.generic import RedirectView
 
-from . import views
+from . import views, app_settings
 
 urlpatterns = patterns(
     "",
@@ -37,12 +37,15 @@ urlpatterns = patterns(
     url(r"^password/reset/key/done/$", views.password_reset_from_key_done,
         name="account_reset_password_from_key_done"),
 
-    # two factor authentication
-    url(r"^two_factor/setup$", views.two_factor_setup,
-        name="two-factor-setup"),
-    url(r"^two_factor/backup_tokens$", views.two_factor_backup_tokens,
-        name="two-factor-backup-tokens"),
-    url(r"^two_factor/qr_code$", views.two_factor_qr_code_generator,
-        name="two-factor-qr-code"),
 
 )
+
+if app_settings.TWO_FACTOR_ENABLED:
+    urlpatterns = patterns('',
+        url(r"^two_factor/setup$", views.two_factor_setup,
+            name="two-factor-setup"),
+        url(r"^two_factor/backup_tokens$", views.two_factor_backup_tokens,
+            name="two-factor-backup-tokens"),
+        url(r"^two_factor/qr_code$", views.two_factor_qr_code_generator,
+            name="two-factor-qr-code"),
+    )

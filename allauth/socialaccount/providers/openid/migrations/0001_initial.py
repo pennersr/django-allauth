502e07b3065d597ac9d769523db428c2f839d34f
Raymond Penners <raymond.penners@intenct.nl>
2015-03-28 10:56:07 +0100

    OpenID migrations missing

---
+++ allauth/socialaccount/providers/openid/migrations/0001_initial.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='OpenIDNonce',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('server_url', models.CharField(max_length=255)),
+                ('timestamp', models.IntegerField()),
+                ('salt', models.CharField(max_length=255)),
+                ('date_created', models.DateTimeField(auto_now_add=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='OpenIDStore',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('server_url', models.CharField(max_length=255)),
+                ('handle', models.CharField(max_length=255)),
+                ('secret', models.TextField()),
+                ('issued', models.IntegerField()),
+                ('lifetime', models.IntegerField()),
+                ('assoc_type', models.TextField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]

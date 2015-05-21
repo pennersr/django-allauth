371bac5666350f86bf6ad41a4927215e5fbc1f81
Nabeel Valapra <nabeelvalapra@gmail.com>
2015-05-21 11:09:48 +0530

    also compatible with django 1.8 and below

--- allauth/account/utils.py
+++ allauth/account/utils.py
@@ -5,6 +5,7 @@ except ImportError:
     from datetime import datetime
     now = datetime.now
 
+import django
 from django.contrib import messages
 from django.core.urlresolvers import reverse
 from django.db.models import Q
@@ -15,7 +16,11 @@ from django.utils.http import urlencode
 from django.utils.http import int_to_base36, base36_to_int
 from django.core.exceptions import ValidationError
 
-from collections import OrderedDict
+if django.VERSION > (1, 8,):
+    from collections import OrderedDict
+else:
+    from django.utils.datastructures import SortedDict as OrderedDict
+
 try:
     from django.utils.encoding import force_text
 except ImportError:

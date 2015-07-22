6b165be6b7fedd74732b789d1a55ec7edfc2f298
Raymond Penners <raymond.penners@intenct.nl>
2015-07-22 23:41:09 +0200

    BinaryField fix for py3.2 (again)

--- allauth/utils.py
+++ allauth/utils.py
@@ -191,7 +191,7 @@ def serialize_instance(instance):
             continue
         try:
             if isinstance(instance._meta.get_field(k), BinaryField):
-                v = force_text(base64.b64encode(force_bytes(v)))
+                v = force_text(base64.b64encode(v))
         except FieldDoesNotExist:
             pass
         data[k] = v
@@ -211,7 +211,9 @@ def deserialize_instance(model, data):
                 elif isinstance(f, DateField):
                     v = dateparse.parse_date(v)
                 elif isinstance(f, BinaryField):
-                    v = force_bytes(base64.b64decode(v))
+                    v = force_bytes(
+                        base64.b64decode(
+                            force_bytes(v)))
             except FieldDoesNotExist:
                 pass
         setattr(ret, k, v)

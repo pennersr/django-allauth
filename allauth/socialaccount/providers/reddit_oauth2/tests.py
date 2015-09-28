d89b508fc0fbceba57349f5fdff4d05ce54541f2
Wendy Edwards <mrscake@gmail.com>
2015-09-28 15:06:49 -0500

    Update tests.py

--- allauth/socialaccount/providers/reddit_oauth2/tests.py
+++ allauth/socialaccount/providers/reddit_oauth2/tests.py
@@ -12,15 +12,4 @@ class RedditOAuth2Tests(create_oauth2_tests(registry.by_id(
         RedditOAuth2Provider.id))):
     def get_mocked_response(self):
         return [MockedResponse(200, """{
-        u'name': u'wayward710', 
-        u'created': 1294876805.0, 
-        u'hide_from_robots': False, u'gold_creddits': 0, 
-        u'created_utc': 1294876805.0, u'link_karma': 1, u'comment_karma': 0, 
-        u'over_18': False, 
-        u'is_gold': False, 
-        u'is_mod': False, 
-        u'gold_expiration': None, 
-        u'has_verified_email': False, 
-        u'id': 
-        u'4pxw0', 
-        u'inbox_count': 0}""")]
+        "name": "wayward710"}""")]

f60ca11d1fe2595750c6b22afd701f2f534a952f
Andrey Kolpakov <aakolpakov@gmail.com>
2015-11-11 13:32:44 +0100

    Stripe OAuth2 provider (issue #1186)

---
+++ allauth/socialaccount/providers/stripe/views.py
@@ -0,0 +1,24 @@
+import requests
+
+from allauth.socialaccount.providers.oauth2.views import (OAuth2Adapter,
+                                                          OAuth2LoginView,
+                                                          OAuth2CallbackView)
+from .provider import StripeProvider
+
+
+class StripeOAuth2Adapter(OAuth2Adapter):
+    provider_id = StripeProvider.id
+    access_token_url = 'https://connect.stripe.com/oauth/token'
+    authorize_url = 'https://connect.stripe.com/oauth/authorize'
+    profile_url = 'https://api.stripe.com/v1/accounts/%s'
+
+    def complete_login(self, request, app, token, response, **kwargs):
+        resp = requests.get(self.profile_url % response.get('stripe_user_id'),
+                            params={'access_token': token.token})
+        extra_data = resp.json()
+        return self.get_provider().sociallogin_from_response(request,
+                                                             extra_data)
+
+
+oauth2_login = OAuth2LoginView.adapter_view(StripeOAuth2Adapter)
+oauth2_callback = OAuth2CallbackView.adapter_view(StripeOAuth2Adapter)

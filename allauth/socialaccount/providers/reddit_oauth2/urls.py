decc405a876e8c2a81b5ef909467b2fc47b4bf05
wayward710 <mrscake@gmail.com>
2015-11-17 02:22:18 -0600

    Fixed issue with oauth/oauth2 url patterns

--- allauth/socialaccount/providers/reddit_oauth2/urls.py
+++ allauth/socialaccount/providers/reddit_oauth2/urls.py
@@ -1,4 +1,4 @@
-from allauth.socialaccount.providers.oauth.urls import default_urlpatterns
+from allauth.socialaccount.providers.oauth2.urls import default_urlpatterns
 
 from provider import RedditOAuth2Provider
 

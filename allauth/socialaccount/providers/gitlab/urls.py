24fcdd96da6014f62ec30a113110a8dbe52cfad4
Daniel Widerin <daniel@widerin.net>
2015-12-14 23:57:08 +0100

    Avoid relative import

--- allauth/socialaccount/providers/gitlab/urls.py
+++ allauth/socialaccount/providers/gitlab/urls.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
+from allauth.socialaccount.providers.gitlab.provider import GitLabProvider
 from allauth.socialaccount.providers.oauth2.urls import default_urlpatterns
-from .provider import GitLabProvider
+
 
 urlpatterns = default_urlpatterns(GitLabProvider)

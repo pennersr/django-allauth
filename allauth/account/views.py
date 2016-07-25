d70bf2eb2cd24e6f8debcd0ba0565f2d66f52322
Raymond Penners <raymond.penners@intenct.nl>
2016-07-25 14:48:25 +0200

    Fix KeyError when ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE

--- allauth/account/views.py
+++ allauth/account/views.py
@@ -198,8 +198,13 @@ class SignupView(RedirectAuthenticatedUserMixin, CloseableSignupMixin,
     def get_context_data(self, **kwargs):
         ret = super(SignupView, self).get_context_data(**kwargs)
         form = ret['form']
-        form.fields["email"].initial = self.request.session \
-            .get('account_verified_email')
+        email = self.request.session.get('account_verified_email')
+        if app_settings.SIGNUP_EMAIL_ENTER_TWICE:
+            email_keys = ['email1', 'email2']
+        else:
+            email_keys = ['email']
+        for email_key in email_keys:
+            form.fields[email_key].initial = email
         login_url = passthrough_next_redirect_url(self.request,
                                                   reverse("account_login"),
                                                   self.redirect_field_name)

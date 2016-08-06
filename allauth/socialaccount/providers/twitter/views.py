28c90c3c5f42c7f8740d57819ad962fc7a504607
Dan Mirsky <mirskiy@gmail.com>
2016-08-05 20:19:11 -0700

    Added email to Twitter provider
    If QUERY_EMAIL is set, append include_email to twitter user profile url
    Always try to get the email from the response, will be None if it isn't
    NOTE: If user doesn't have an email in Twitter, email field in response
    will be a blank string "", not None

--- allauth/socialaccount/providers/twitter/views.py
+++ allauth/socialaccount/providers/twitter/views.py
@@ -1,5 +1,6 @@
 import json
 
+from allauth.socialaccount.app_settings import QUERY_EMAIL
 from allauth.socialaccount.providers.oauth.client import OAuth
 from allauth.socialaccount.providers.oauth.views import (OAuthAdapter,
                                                          OAuthLoginView,
@@ -12,7 +13,8 @@ class TwitterAPI(OAuth):
     """
     Verifying twitter credentials
     """
-    url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
+    _base_url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
+    url = _base_url + '?include_email=true' if QUERY_EMAIL else _base_url
 
     def get_user_info(self):
         user = json.loads(self.query(self.url))

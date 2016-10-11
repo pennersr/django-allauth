4f57eece7857cee83153061fcb3d5e9fc9414c24
Jerome Leclanche <jerome@leclan.ch>
2016-10-12 02:35:48 +0300

    battlenet: Add a test against expected `extra_data`

--- allauth/socialaccount/providers/battlenet/tests.py
+++ allauth/socialaccount/providers/battlenet/tests.py
@@ -1,3 +1,5 @@
+import json
+from allauth.socialaccount.models import SocialAccount
 from allauth.socialaccount.providers.oauth2.client import OAuth2Error
 from allauth.socialaccount.tests import OAuth2TestsMixin
 from allauth.tests import MockedResponse, TestCase
@@ -7,13 +9,12 @@ from .views import _check_errors
 
 class BattleNetTests(OAuth2TestsMixin, TestCase):
     provider_id = BattleNetProvider.id
+    _uid = 123456789
+    _battletag = "LuckyDragon#1953"
 
     def get_mocked_response(self):
-        return MockedResponse(200, """
-        {
-            "battletag": "LuckyDragon#1953",
-            "id": "123456789"
-        }""")
+        data = {"battletag": self._battletag, "id": self._uid}
+        return MockedResponse(200, json.dumps(data))
 
     def test_invalid_data(self):
         with self.assertRaises(OAuth2Error):
@@ -24,3 +25,10 @@ class BattleNetTests(OAuth2TestsMixin, TestCase):
             _check_errors({"error": "invalid_token"})
 
         _check_errors({"id": 12345})  # Does not raise
+
+    def test_extra_data(self):
+        self.login(self.get_mocked_response())
+        account = SocialAccount.objects.get(uid=str(self._uid))
+        self.assertEqual(account.extra_data["battletag"], self._battletag)
+        self.assertEqual(account.extra_data["id"], self._uid)
+        self.assertEqual(account.extra_data["region"], "us")

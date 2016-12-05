3e4f6a115dd4e983e7610283a41c992470218308
William Li <wli@williamhli.com>
2016-12-04 22:49:51 -0800

    Add support for flickr accounts without a real name (#1551)

--- allauth/socialaccount/providers/flickr/provider.py
+++ allauth/socialaccount/providers/flickr/provider.py
@@ -13,9 +13,17 @@ class FlickrAccount(ProviderAccount):
 
     def to_str(self):
         dflt = super(FlickrAccount, self).to_str()
+
+        # Try to use name if it exists. If there is no name, the Flickr API
+        # returns an empty stirng.
         name = self.account.extra_data \
-            .get('person').get('realname').get('_content', dflt)
-        return name
+            .get('person').get('realname').get('_content', None)
+        if name:
+            return name
+
+        # Default to username if name does not exist.
+        return self.account.extra_data \
+            .get('person').get('username').get('_content', dflt)
 
 
 class FlickrProvider(OAuthProvider):

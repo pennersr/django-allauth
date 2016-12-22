dbf0fedebee3d3aa152ba6a8a9f8d4139c179bfb
Shane <shane@shanerice.me>
2016-12-22 01:25:17 -0800

    Added Discord test

--- allauth/socialaccount/providers/discord/tests.py
+++ allauth/socialaccount/providers/discord/tests.py
@@ -1,38 +1,18 @@
-# -*- coding: utf-8 -*-
-from allauth.socialaccount.providers.gitlab.provider import GitLabProvider
+from allauth.socialaccount.providers.discord.provider import DiscordProvider
 from allauth.socialaccount.tests import OAuth2TestsMixin
 from allauth.tests import MockedResponse
 from allauth.tests import TestCase
 
 
-class GitLabTests(OAuth2TestsMixin, TestCase):
-    provider_id = GitLabProvider.id
+class DiscordTests(OAuth2TestsMixin, TestCase):
+    provider_id = DiscordProvider.id
 
     def get_mocked_response(self):
-        return MockedResponse(200, """
-            {
-                "avatar_url": "https://secure.gravatar.com/avatar/123",
-                "bio": "",
-                "can_create_group": "true",
-                "can_create_project": "true",
-                "color_scheme_id": 2,
-                "created_at": "2015-12-14T23:40:33+0100",
-                "current_sign_in_at": "2015-12-14T23:40:33+0100",
-                "email": "mr.bob@your.gitlab.server.tld",
-                "id": 2,
-                "identities": [],
-                "is_admin": "false",
-                "linkedin": "",
-                "name": "Mr Bob",
-                "private_token": "123",
-                "projects_limit": 10,
-                "skype": "mr.bob",
-                "state": "active",
-                "theme_id": 6,
-                "twitter": "mrbob",
-                "two_factor_enabled": "false",
-                "username": "mr.bob",
-                "web_url": "https://your.gitlab.server.tld/u/mr.bob",
-                "website_url": "http://mr.bob"
-            }
-        """)
+        return MockedResponse(200, """{
+            "id": "80351110224678912",
+            "username": "Nelly",
+            "discriminator": "1337",
+            "avatar": "8342729096ea3675442027381ff50dfe",
+            "verified": true,
+            "email": "nelly@discordapp.com"
+        }""")

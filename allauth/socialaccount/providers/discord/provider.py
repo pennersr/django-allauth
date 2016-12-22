9e851dab74d75ce9d1a4c9385b68d545e401c97e
Shane <shane@shanerice.me>
2016-12-22 01:20:49 -0800

    Adding Discord

---
+++ allauth/socialaccount/providers/discord/provider.py
@@ -0,0 +1,36 @@
+from allauth.socialaccount import providers
+from allauth.socialaccount.providers.base import ProviderAccount
+from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+class DiscordAccount(ProviderAccount):
+    def to_str(self):
+        dflt = super(DiscordAccount, self).to_str()
+        return self.account.extra_data.get('username', dflt)
+
+
+class DiscordProvider(OAuth2Provider):
+    id = 'discord'
+    name = 'Discord'
+    account_class = DiscordAccount
+
+    def extract_uid(self, data):
+        logger.info('data')
+        logger.info(data)
+        return str(data['id'])
+
+    def extract_common_fields(self, data):
+        return dict(
+            email=data.get('email'),
+            username=data.get('username'),
+            name=data.get('username'),
+        )
+
+    def get_default_scope(self):
+        return ['email', 'identify']
+
+
+providers.registry.register(DiscordProvider)
---
+++ allauth/socialaccount/providers/discord/urls.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from allauth.socialaccount.providers.discord.provider import DiscordProvider
+from allauth.socialaccount.providers.oauth2.urls import default_urlpatterns
+
+
+urlpatterns = default_urlpatterns(DiscordProvider)

6e5c707606cd5d64c4163395015ae9157aad7f8e
smashedr <shane@cssnr.com>
2016-12-29 13:21:10 -0800

    Adding GitHub Enterprise Support (#1576)

--- allauth/socialaccount/providers/github/views.py
+++ allauth/socialaccount/providers/github/views.py
@@ -1,32 +1,41 @@
 import requests
-
-from allauth.socialaccount.providers.oauth2.views import (OAuth2Adapter,
-                                                          OAuth2LoginView,
-                                                          OAuth2CallbackView)
-from .provider import GitHubProvider
 from allauth.socialaccount import app_settings
+from allauth.socialaccount.providers.github.provider import GitHubProvider
+from allauth.socialaccount.providers.oauth2.views import OAuth2Adapter
+from allauth.socialaccount.providers.oauth2.views import OAuth2CallbackView
+from allauth.socialaccount.providers.oauth2.views import OAuth2LoginView
 
 
 class GitHubOAuth2Adapter(OAuth2Adapter):
     provider_id = GitHubProvider.id
-    access_token_url = 'https://github.com/login/oauth/access_token'
-    authorize_url = 'https://github.com/login/oauth/authorize'
-    profile_url = 'https://api.github.com/user'
-    emails_url = 'https://api.github.com/user/emails'
+    settings = app_settings.PROVIDERS.get(provider_id, {})
+
+    if 'GITHUB_URL' in settings:
+        web_url = settings.get('GITHUB_URL').rstrip('/')
+        api_url = '{0}/api/v3'.format(web_url)
+    else:
+        web_url = 'https://github.com'
+        api_url = 'https://api.github.com'
+
+    access_token_url = '{0}/login/oauth/access_token'.format(web_url)
+    authorize_url = '{0}/login/oauth/authorize'.format(web_url)
+    profile_url = '{0}/user'.format(api_url)
+    emails_url = '{0}/user/emails'.format(api_url)
 
     def complete_login(self, request, app, token, **kwargs):
-        resp = requests.get(self.profile_url,
-                            params={'access_token': token.token})
+        params = {'access_token': token.token}
+        resp = requests.get(self.profile_url, params=params)
         extra_data = resp.json()
         if app_settings.QUERY_EMAIL and not extra_data.get('email'):
             extra_data['email'] = self.get_email(token)
-        return self.get_provider().sociallogin_from_response(request,
-                                                             extra_data)
+        return self.get_provider().sociallogin_from_response(
+            request, extra_data
+        )
 
     def get_email(self, token):
         email = None
-        resp = requests.get(self.emails_url,
-                            params={'access_token': token.token})
+        params = {'access_token': token.token}
+        resp = requests.get(self.emails_url, params=params)
         emails = resp.json()
         if resp.status_code == 200 and emails:
             email = emails[0]

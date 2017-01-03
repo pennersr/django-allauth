795dbe13199bf53dc039fb5d19f3a6ca3f0ab2ca
Sanghyeok Lee <shlee322@elab.kr>
2017-01-03 21:16:52 +0900

    Fix kakao provider flake8 error

--- allauth/socialaccount/providers/kakao/provider.py
+++ allauth/socialaccount/providers/kakao/provider.py
@@ -6,12 +6,13 @@ from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
 class KakaoAccount(ProviderAccount):
 
     def get_avatar_url(self):
-        return self.account.extra_data.get('properties',
-                                           {}).get('thumbnail_image')
+        return self.account.extra_data.get(
+            'properties', {}).get('thumbnail_image')
 
     def to_str(self):
-        return self.account.extra_data.get('properties',
-                                           {}).get('nickname', self.account.uid)
+        return self.account.extra_data.get(
+            'properties', {}).get(
+            'nickname', self.account.uid)
 
 
 class KakaoProvider(OAuth2Provider):

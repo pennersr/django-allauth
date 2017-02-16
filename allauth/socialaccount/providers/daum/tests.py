e7ae7c1e9dfba0391f3adc55827fc277c68ca2ac
junh.kim <junh.kim@ohmytrip.com>
2017-02-16 19:41:45 +0900

    break long string

--- allauth/socialaccount/providers/daum/tests.py
+++ allauth/socialaccount/providers/daum/tests.py
@@ -8,18 +8,18 @@ class DaumTests(OAuth2TestsMixin, TestCase):
     provider_id = DaumProvider.id
 
     def get_mocked_response(self):
-
-        return MockedResponse(200, """
-{
-"code": 200,
-"message": "OK",
-"result": {
-"userid": "38DTh",
-"id": 46287445,
-"nickname": "xncbf",
-"imagePath": "https://img1.daumcdn.net/thumb/R55x55/?fname=http%3A%2F%2Ftwg.tset.daumcdn.net%2Fprofile%2F-zYQhBX2b-E0&t=1487231948435",
-"bigImagePath": "https://img1.daumcdn.net/thumb/R158x158/?fname=http%3A%2F%2Ftwg.tset.daumcdn.net%2Fprofile%2F-zYQhBX2b-E0&t=1487231948435",
-"openProfile": true
-}
-}
-""")
+        return MockedResponse(200, '{' +
+'"code": 200,' +
+'"message": "OK",' +
+'"result": {' +
+'"userid": "38DTh",' +
+'"id": 46287445,' +
+'"nickname": "xncbf",' +
+'"imagePath": "https://img1.daumcdn.net/thumb/R55x55/?fname=http%3A%2F%2F' +
+'twg.tset.daumcdn.net%2Fprofile%2F-zYQhBX2b-E0&t=1487231948435",' +
+'"bigImagePath": "https://img1.daumcdn.net/thumb/R158x158/' +
+'?fname=http%3A%2F%2Ftwg.tset.daumcdn.net' +
+'%2Fprofile%2F-zYQhBX2b-E0&t=1487231948435",' +
+'"openProfile": true' +
+'}' +
+'}')

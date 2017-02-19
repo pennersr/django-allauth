a2e7ab7a55b3877c6f5858c5638363127c1372da
Ben Timby <btimby@smartfile.com>
2017-02-19 00:49:00 -0500

    Add scope handling for Onedrive.

--- allauth/socialaccount/providers/onedrive/provider.py
+++ allauth/socialaccount/providers/onedrive/provider.py
@@ -3,6 +3,11 @@ from allauth.socialaccount.providers.base import ProviderAccount
 from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
 
 
+class Scope(object):
+    PROFILE = 'wl.basic'
+    READWRITE = 'onedrive.readwrite'
+
+
 class OnedriveOAuth2Account(ProviderAccount):
     pass
 
@@ -12,8 +17,12 @@ class OnedriveOAuth2Provider(OAuth2Provider):
     name = 'Onedrive'
     account_class = OnedriveOAuth2Account
 
+    def get_default_scope(self):
+        scope = [Scope.PROFILE, Scope.READWRITE]
+        return scope
+
     def extract_uid(self, data):
-        return data['uid']
+        return data['id']
 
     def extract_common_fields(self, data):
         return dict(name=data.get('display_name'),

6c6563f612c0ce3d9e154b6758e548ca5794a948
Raymond Penners <raymond.penners@intenct.nl>
2017-02-28 14:41:03 +0100

    Add user_logged_out signal
    Closes #1087

--- allauth/account/signals.py
+++ allauth/account/signals.py
@@ -2,6 +2,7 @@ from django.dispatch import Signal
 
 
 user_logged_in = Signal(providing_args=["request", "user"])
+user_logged_out = Signal(providing_args=["request", "user"])
 
 # Typically followed by `user_logged_in` (unless, e-mail verification kicks in)
 user_signed_up = Signal(providing_args=["request", "user"])

d1cfbb73ea82333420824cf0ba262e498f7cbf0d
Rishabh Agrahari <rishabh.agrahari.eee15@iitbhu.ac.in>
2017-03-16 18:03:20 +0530

    fixed forms.py

--- allauth/account/forms.py
+++ allauth/account/forms.py
@@ -161,7 +161,8 @@ class LoginForm(forms.Form):
                 raise forms.ValidationError(
                     self.error_messages[error_type])
             else:
-                raise forms.ValidationError(error_messages[
+                raise forms.ValidationError(
+                    self.error_messages[
                     '%s_password_mismatch'
                     % app_settings.AUTHENTICATION_METHOD])
         return self.cleaned_data

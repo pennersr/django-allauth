bac90d4bb67c4b06f22c5bf7cd08a510eecaa38d
Griffith Rees <griff.rees@gmail.com>
2017-03-31 17:27:03 +0100

    Possible fix of EventbriteAccount get_profile_url and get_avatar_url

--- allauth/socialaccount/providers/eventbrite/provider.py
+++ allauth/socialaccount/providers/eventbrite/provider.py
@@ -3,21 +3,45 @@ from allauth.account.models import EmailAddress
 from allauth.socialaccount.providers.base import ProviderAccount
 from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
 
-
-PROFILE_URL = 'https://www.eventbriteapi.com/v3/users/me/'
+API_URL = 'https://www.eventbriteapi.com/v3/'
 
 
 class EventbriteAccount(ProviderAccount):
 
     """ProviderAccount subclass for Eventbrite."""
 
-    def get_avatar_url(self):
-        """Return avatar url."""
-        return self.account.extra_data['image_id']
-
     def get_profile_url(self):
-        """Return profile URL (same for EventbriteOAuth2Adapter)."""
-        return PROFILE_URL
+        """
+        Return a URL for a user profile irrespective of their login status.
+
+        NB: This URL by itself will not provide access. It must be
+        combined with the user's token either via ?token=user's_token (where
+        user's_token is their token string) or via the following example:
+
+        >>> import requests
+        >>> headers = {'Authorization': 'Bearer {0}'.format("user's_token")}
+        >>> profile_url = API_URL + 'users/' + self.account.extra_data['id']
+        >>> profile = requests.get(profile_url).json()
+        """
+        return API_URL + 'users/' + self.account.extra_data['id']
+
+    def get_avatar_url(self):
+        """
+        Return avatar data endpoint.
+
+        NB: This doesn't currently return a image file URL, but an endpoint for
+        the user's avatar data. The actual image url can be queried from this
+        endpoint via the 'url' key, but the user's Eventbrite token must either
+        be included in the header or in the url itself.
+
+        >>> import requests
+        >>> headers = {'Authorization': 'Bearer {0}'.format("user's_token")}
+        >>> image_url_endpoint = (API_URL + 'media/' +
+        ...                       self.account.extra_data['image_id'])
+        >>> response = requests.get(image_url_endpoint)
+        >>> url = response.json()['url']
+        """
+        return API_URL + 'media/' + self.account.extra_data['image_id']
 
     def to_str(self):
         """A wrapper method for __str__ for python 2 compatibility."""
@@ -38,14 +62,14 @@ class EventbriteProvider(OAuth2Provider):
         return str(data['id'])
 
     def extract_common_fields(self, data):
-        """Extract fields from PROFILE_URL."""
+        """Extract fields from a default query of v3/users/me"""
         return dict(
             emails=data.get('emails'),
             id=data.get('id'),
             name=data.get('name'),
             first_name=data.get('first_name'),
             last_name=data.get('last_name'),
-            image_url=data.get('image_url')
+            image_id=data.get('image_id')
         )
 
     def extract_email_addresses(self, data):
--- allauth/socialaccount/providers/eventbrite/views.py
+++ allauth/socialaccount/providers/eventbrite/views.py
@@ -7,7 +7,7 @@ from allauth.socialaccount.providers.oauth2.views import (
     OAuth2LoginView,
 )
 
-from .provider import PROFILE_URL, EventbriteProvider
+from .provider import EventbriteProvider, API_URL
 
 
 class EventbriteOAuth2Adapter(OAuth2Adapter):
@@ -18,11 +18,12 @@ class EventbriteOAuth2Adapter(OAuth2Adapter):
 
     authorize_url = 'https://www.eventbrite.com/oauth/authorize'
     access_token_url = 'https://www.eventbrite.com/oauth/token'
-    profile_url = PROFILE_URL
+    profile_url = API_URL + 'users/me/'
 
     def complete_login(self, request, app, token, **kwargs):
-        """Complete login."""
-        resp = requests.get(self.profile_url, params={'token': token.token})
+        """Complete login using headers."""
+        headers = {'Authorization': 'Bearer {0}'.format(token.token)}
+        resp = requests.get(self.profile_url, headers=headers)
         resp.raise_for_status()
         extra_data = resp.json()
         return self.get_provider().sociallogin_from_response(request,

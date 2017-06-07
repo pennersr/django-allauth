e2300911788c8db7eda6f85f9a0a5e7f6080f5df
Ryan Verner <ryan.verner@gmail.com>
2017-06-07 16:35:26 +1000

    Finalise Patreon provider, add scope, profile_url, tests, ensure all working

--- test_settings.py
+++ test_settings.py
@@ -96,6 +96,7 @@ INSTALLED_APPS = (
     'allauth.socialaccount.providers.odnoklassniki',
     'allauth.socialaccount.providers.openid',
     'allauth.socialaccount.providers.orcid',
+    'allauth.socialaccount.providers.patreon',
     'allauth.socialaccount.providers.paypal',
     'allauth.socialaccount.providers.persona',
     'allauth.socialaccount.providers.pinterest',

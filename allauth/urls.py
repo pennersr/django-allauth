051e901f6c2baaee38d17876640da407214e5b78
wardbradt <bbradt@andover.edu>
2017-12-05 18:10:23 -0500

    update all urls.py files to Django 2.0.

--- allauth/urls.py
+++ allauth/urls.py
@@ -1,16 +1,17 @@
 from importlib import import_module
 
 from django.conf.urls import include, url
+from django.urls import path
 
 from allauth.socialaccount import providers
 
 from . import app_settings
 
 
-urlpatterns = [url(r'^', include('allauth.account.urls'))]
+urlpatterns = [path(r'', include('allauth.account.urls'))]
 
 if app_settings.SOCIALACCOUNT_ENABLED:
-    urlpatterns += [url(r'^social/', include('allauth.socialaccount.urls'))]
+    urlpatterns += [path('social/', include('allauth.socialaccount.urls'))]
 
 for provider in providers.registry.get_list():
     try:

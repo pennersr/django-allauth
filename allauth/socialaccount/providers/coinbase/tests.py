f7d9a8647f051a3e34df42d0336fe32b4bdd2154
dunowat <jvilaikeo@gmail.com>
2018-01-08 20:47:28 -0800

    update test info

--- allauth/socialaccount/providers/coinbase/tests.py
+++ allauth/socialaccount/providers/coinbase/tests.py
@@ -9,39 +9,45 @@ class CoinbaseTests(OAuth2TestsMixin, TestCase):
 
     def get_mocked_response(self):
         return MockedResponse(200, """
+
+
 {
-  "users": [
     {
-      "user": {
-        "id": "512db383f8182bd24d000001",
-        "name": "User One",
-        "email": "user1@example.com",
-        "time_zone": "Pacific Time (US & Canada)",
-        "native_currency": "USD",
-        "balance": {
-          "amount": "49.76000000",
-          "currency": "BTC"
-        },
-        "merchant": {
-          "company_name": "Company Name, Inc.",
-          "logo": {
-            "small": "http://smalllogo.example",
-            "medium": "http://mediumlogo.example",
-            "url": "http://logo.example"
-          }
-        },
-        "buy_level": 1,
-        "sell_level": 1,
-        "buy_limit": {
-          "amount": "10.00000000",
-          "currency": "BTC"
-        },
-        "sell_limit": {
-          "amount": "100.00000000",
-          "currency": "BTC"
-        }
-      }
+  "data": [
+    {
+      "id": "58542935-67b5-56e1-a3f9-42686e07fa40",
+      "name": "My Vault",
+      "primary": false,
+      "type": "vault",
+      "currency": "BTC",
+      "balance": {
+        "amount": "4.00000000",
+        "currency": "BTC"
+      },
+      "created_at": "2015-01-31T20:49:02Z",
+      "updated_at": "2015-01-31T20:49:02Z",
+      "resource": "account",
+      "resource_path": "/v2/accounts/58542935-67b5-56e1-a3f9-42686e07fa40",
+      "ready": true
+    },
+    {
+      "id": "2bbf394c-193b-5b2a-9155-3b4732659ede",
+      "name": "My Wallet",
+      "primary": true,
+      "type": "wallet",
+      "currency": "BTC",
+      "balance": {
+        "amount": "39.59000000",
+        "currency": "BTC"
+      },
+      "created_at": "2015-01-31T20:49:02Z",
+      "updated_at": "2015-01-31T20:49:02Z",
+      "resource": "account",
+      "resource_path": "/v2/accounts/2bbf394c-193b-5b2a-9155-3b4732659ede"
     }
-  ]
+    ]
+  }
 }
         """)
+
+

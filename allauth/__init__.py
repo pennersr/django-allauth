f3591bfc253fd48becbb190487bcd94740e7dece
Raymond Penners <raymond.penners@intenct.nl>
2018-02-02 19:56:42 +0100

    chore: Release 0.35.0

--- allauth/__init__.py
+++ allauth/__init__.py
@@ -8,7 +8,7 @@ r"""
 
 """
 
-VERSION = (0, 34, 0, 'final', 0)
+VERSION = (0, 35, 0, 'final', 0)
 
 __title__ = 'django-allauth'
 __version_info__ = VERSION
@@ -16,4 +16,4 @@ __version__ = '.'.join(map(str, VERSION[:3])) + ('-{}{}'.format(
     VERSION[3], VERSION[4] or '') if VERSION[3] != 'final' else '')
 __author__ = 'Raymond Penners'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2010-2017 Raymond Penners and contributors'
+__copyright__ = 'Copyright 2010-2018 Raymond Penners and contributors'

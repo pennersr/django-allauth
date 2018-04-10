96cf384c272bed81c4368ac50d2e02bd5a79399f
Raymond Penners <raymond.penners@intenct.nl>
2018-04-10 23:07:09 +0200

    fix(django): allowed_hosts is now a required argument of is_safe_url

--- allauth/account/adapter.py
+++ allauth/account/adapter.py
@@ -407,7 +407,7 @@ class DefaultAccountAdapter(object):
 
     def is_safe_url(self, url):
         from django.utils.http import is_safe_url
-        return is_safe_url(url)
+        return is_safe_url(url, allowed_hosts=None)
 
     def get_email_confirmation_url(self, request, emailconfirmation):
         """Constructs the email confirmation (activation) url.

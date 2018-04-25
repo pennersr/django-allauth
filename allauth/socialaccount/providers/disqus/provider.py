e5e4f5321d636a31c789cf1186bfc4e8dc7d0e18
Rolando Urquiza <rolurquiza@gmail.com>
2018-04-25 10:17:47 -0400

    Fix import sorting

--- allauth/socialaccount/providers/disqus/provider.py
+++ allauth/socialaccount/providers/disqus/provider.py
@@ -1,7 +1,7 @@
 from allauth.account.models import EmailAddress
-from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
-from allauth.socialaccount.providers.base import ProviderAccount
 from allauth.socialaccount.app_settings import QUERY_EMAIL
+from allauth.socialaccount.providers.base import ProviderAccount
+from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
 
 
 class DisqusAccount(ProviderAccount):
--- allauth/socialaccount/providers/disqus/urls.py
+++ allauth/socialaccount/providers/disqus/urls.py
@@ -2,4 +2,5 @@ from allauth.socialaccount.providers.oauth2.urls import default_urlpatterns
 
 from .provider import DisqusProvider
 
+
 urlpatterns = default_urlpatterns(DisqusProvider)

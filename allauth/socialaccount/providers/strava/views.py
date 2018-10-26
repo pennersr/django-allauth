668639029120186ddf650106e30ffb612d18751c
LittleBigFrog <Medor@Medors-iMac.local>
2018-10-26 15:01:26 +0100

    flake8 update

--- allauth/socialaccount/providers/strava/views.py
+++ allauth/socialaccount/providers/strava/views.py
@@ -1,20 +1,31 @@
 import requests
 
-from allauth.socialaccount.providers.oauth2.views import OAuth2Adapter, OAuth2CallbackView, OAuth2LoginView
+from allauth.socialaccount.providers.oauth2.views import OAuth2Adapter, \
+    OAuth2CallbackView, OAuth2LoginView
 
-from .provider import StravaProvider
+from provider import StravaProvider
 
 
 class StravaAdapter(OAuth2Adapter):
-    provider_id = StravaProvider.id
-    access_token_url = "https://www.strava.com/oauth/token"
-    authorize_url = "https://www.strava.com/oauth/authorize"
-    profile_url = "https://www.strava.com/api/v3/athlete"
 
-    def complete_login(self, request, app, token, **kwargs):
-        resp = requests.get(self.profile_url, headers={"Authorization": "Bearer " + token.token})
+    provider_id = StravaProvider.id
+    access_token_url = 'https://www.strava.com/oauth/token'
+    authorize_url = 'https://www.strava.com/oauth/authorize'
+    profile_url = 'https://www.strava.com/api/v3/athlete'
+
+    def complete_login(
+        self,
+        request,
+        app,
+        token,
+        **kwargs
+        ):
+        resp = requests.get(self.profile_url,
+                            headers={'Authorization': 'Bearer '
+                            + token.token})
         extra_data = resp.json()
-        return self.get_provider().sociallogin_from_response(request, extra_data)
+        return self.get_provider().sociallogin_from_response(request,
+                extra_data)
 
 
 oauth2_login = OAuth2LoginView.adapter_view(StravaAdapter)

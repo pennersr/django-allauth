db7adf9f86ac563602473c523600a21e1a6ec72a
Bas <bas@tenberge-ict.nl>
2019-03-05 20:55:40 +0100

    Fixed the isort testcase

--- allauth/socialaccount/providers/linkedin_oauth2/tests.py
+++ allauth/socialaccount/providers/linkedin_oauth2/tests.py
@@ -1,10 +1,9 @@
+from json import loads
+
 from allauth.socialaccount.models import SocialAccount
 from allauth.socialaccount.tests import OAuth2TestsMixin
 from allauth.tests import MockedResponse, TestCase
-
 from django.test.utils import override_settings
-
-from json import loads
 
 from .provider import LinkedInOAuth2Provider
 

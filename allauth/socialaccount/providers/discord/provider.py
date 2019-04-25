b9b2b3650f924b50333732617913409b32d8f2b5
Raymond Penners <raymond.penners@intenct.nl>
2019-04-25 22:56:08 +0200

    fix(pep8): Code style issue in Discord provider

--- allauth/socialaccount/providers/discord/provider.py
+++ allauth/socialaccount/providers/discord/provider.py
@@ -1,6 +1,7 @@
+from allauth.account.models import EmailAddress
 from allauth.socialaccount.providers.base import ProviderAccount
 from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
-from allauth.account.models import EmailAddress
+
 
 class DiscordAccount(ProviderAccount):
     def to_str(self):

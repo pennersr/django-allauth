6743939d7e2b17d0cf2515a84bf306b0debbb929
Immortalin <Immortalin@icloud.com>
2019-04-28 17:42:48 +0800

    Added Stripe Connect Express support

---
+++ allauth/socialaccount/providers/stripe_express/provider.py
@@ -0,0 +1,27 @@
+from allauth.socialaccount.providers.base import ProviderAccount
+from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
+
+
+class StripeExpressAccount(ProviderAccount):
+    def to_str(self):
+        default = super(StripeExpressAccount, self).to_str()
+        return self.account.extra_data.get('business_name', default)
+
+
+class StripeExpressProvider(OAuth2Provider):
+    id = 'stripe_express'
+    name = 'Stripe Express'
+    account_class = StripeExpressAccount
+
+    def extract_uid(self, data):
+        return data['id']
+
+    def extract_common_fields(self, data):
+        return dict(name=data.get('display_name'),
+                    email=data.get('email'))
+
+    def get_default_scope(self):
+        return ['read_only']
+
+
+provider_classes = [StripeExpressProvider]
---
+++ allauth/socialaccount/providers/stripe_express/views.py
@@ -0,0 +1,28 @@
+import requests
+
+from allauth.socialaccount.providers.oauth2.views import (
+    OAuth2Adapter,
+    OAuth2CallbackView,
+    OAuth2LoginView,
+)
+
+from .provider import StripeExpressProvider
+
+
+class StripeExpressOAuth2Adapter(OAuth2Adapter):
+    provider_id = StripeExpressProvider.id
+    access_token_url = 'https://connect.stripe.com/express/oauth/token'
+    authorize_url = 'https://connect.stripe.com/express/oauth/authorize'
+    profile_url = 'https://api.stripe.com/v1/accounts/%s'
+
+    def complete_login(self, request, app, token, response, **kwargs):
+        headers = {'Authorization': 'Bearer {0}'.format(token.token), 'Stripe-Version': '2019-03-14'}
+        resp = requests.get(self.profile_url % response.get('stripe_user_id'),
+                            headers=headers)
+        extra_data = resp.json()
+        return self.get_provider().sociallogin_from_response(request,
+                                                             extra_data)
+
+
+oauth2_login = OAuth2LoginView.adapter_view(StripeExpressOAuth2Adapter)
+oauth2_callback = OAuth2CallbackView.adapter_view(StripeExpressOAuth2Adapter)

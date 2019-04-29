3237cfaabb212a322f8b32379f37472ff299fae1
Volker Vorwerk <volker.vorwerk@travelperk.com>
2019-04-29 16:35:16 +0200

    fix(socialaccount) SocialLogin is_existing returns boolean

--- allauth/socialaccount/models.py
+++ allauth/socialaccount/models.py
@@ -249,7 +249,7 @@ class SocialLogin(object):
         """
         Account is temporary, not yet backed by a database record.
         """
-        return self.account.pk
+        return self.account.pk is not None
 
     def lookup(self):
         """

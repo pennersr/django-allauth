168c5e3d5e1607cd0ecb10b5fef571645ae13698
PNWParksFan <gtaparks@gmail.com>
2019-05-06 00:06:14 -0700

    More linting fixes

--- allauth/socialaccount/providers/discord/provider.py
+++ allauth/socialaccount/providers/discord/provider.py
@@ -9,7 +9,7 @@ class DiscordAccount(ProviderAccount):
         return self.account.extra_data.get('username', dflt)
 
     def get_avatar_url(self):
-        if ('id' in self.account.extra_data.keys() 
+        if ('id' in self.account.extra_data.keys()
                 and 'avatar' in self.account.extra_data.keys()):
             return 'https://cdn.discordapp.com/avatars/{id}/{avatar}.png'\
                 .format(**self.account.extra_data)

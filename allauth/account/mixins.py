22712c7d5c89f6dcb858602782d59c8588a3cb83
adonis simo <simoadonis@yahoo.fr>
2019-08-19 01:00:30 +0100

    Fix some CI build errors causes
    Tasks:
    - removed some trailing spaces
    - split code in multiple line to reduce the number of char on the line

--- allauth/account/mixins.py
+++ allauth/account/mixins.py
@@ -1,7 +1,7 @@
 """
-This module contain a class mixin design to bring email verification 
+This module contain a class mixin design to bring email verification
 to class based views in django-allauth
-It is used like django's LoginRequiedMixin, so when using this 
+It is used like django's LoginRequiedMixin, so when using this
 there is no need to use LoginRequiedMixin fro django
 
 Example of use :
@@ -15,23 +15,25 @@ class IndexView(VerifiedEmailRequiredMixin, ListView):
     ...
 """
 
-from django.http import HttpResponseRedirect
-from django.urls import reverse
 from django.utils.decorators import method_decorator
+from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
+
 from allauth.account.decorators import verified_email_required
 
 
 class VerifiedEmailRequiredMixin(LoginRequiredMixin):
     """
-    This class is a version of the 
-    allauth.account.decorators.verified_email_required() 
+    This class is a version of the
+    allauth.account.decorators.verified_email_required()
     decorator, it is meant to be use in a class-based view.
     """
 
     @method_decorator(verified_email_required)
     def dispatch(self, request, *args, **kwargs):
         """
-        Just override the dispatch() method to run the control before doing anything in the class
+        Just override the dispatch() method to run
+        the control before doing anything in the class
         """
-        return super(VerifiedEmailRequiredMixin, self).dispatch(request, *args, **kwargs)
+        return super(VerifiedEmailRequiredMixin, self)
+                    .dispatch(request, *args, **kwargs)

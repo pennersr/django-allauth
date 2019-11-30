282cf76e3bffa17eb3a3f3f7bb7e5658550e3537
Francis Brito <fr.br94@gmail.com>
2019-11-30 01:12:16 -0400

    fix(amazon_cognito): fix typo in Amazon Cognito provider urls

--- allauth/socialaccount/providers/amazon_cognito/urls.py
+++ allauth/socialaccount/providers/amazon_cognito/urls.py
@@ -1,4 +1,4 @@
-from allauth.socialaccount.providers.amazon_cognito.provider import AmazonCognitoAccount
+from allauth.socialaccount.providers.amazon_cognito.provider import AmazonCognitoProvider
 from allauth.socialaccount.providers.oauth2.urls import default_urlpatterns
 
-urlpatterns = default_urlpatterns(AmazonCognitoAccount)
+urlpatterns = default_urlpatterns(AmazonCognitoProvider)

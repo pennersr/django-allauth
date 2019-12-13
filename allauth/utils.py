696a840d65de1edd06ddd50a8436ae864fae43f6
Jihoon Park <bartleby2718@gmail.com>
2019-12-13 14:50:34 -0500

    fix(utils): Graceful fallback to default when request=None
    Avoids error when 500 template contains a django-allauth template tag

--- allauth/utils.py
+++ allauth/utils.py
@@ -298,4 +298,6 @@ def get_form_class(forms, form_id, default_form):
 
 
 def get_request_param(request, param, default=None):
+    if request is None:
+        return default
     return request.POST.get(param) or request.GET.get(param, default)

d4a2873f0433025234a84fe26b2d9487bc506b36
Raymond Penners <raymond.penners@intenct.nl>
2019-12-15 20:50:53 +0100

    fix(instagram): Check API response code

--- allauth/socialaccount/providers/instagram/views.py
+++ allauth/socialaccount/providers/instagram/views.py
@@ -18,6 +18,7 @@ class InstagramOAuth2Adapter(OAuth2Adapter):
     def complete_login(self, request, app, token, **kwargs):
         resp = requests.get(self.profile_url,
                             params={'access_token': token.token})
+        resp.raise_for_status()
         extra_data = resp.json()
         return self.get_provider().sociallogin_from_response(request,
                                                              extra_data)

d6702a318f96f2f57a8d3cd88a54ff34b950b4b7
Oleg Sergeev <se1@rep.earth>
2019-12-24 21:52:49 +0300

    email's 'verified' flag submitting updated

--- allauth/socialaccount/providers/apple/provider.py
+++ allauth/socialaccount/providers/apple/provider.py
@@ -20,8 +20,8 @@ class AppleProvider(OAuth2Provider):
     def extract_email_addresses(self, data):
         ret = []
         email = data.get('email')
-        if email and data.get('email_verified'):
-            ret.append(EmailAddress(email=email, verified=True, primary=True))
+        if email:
+            ret.append(EmailAddress(email=email, verified=data.get('email_verified'), primary=True))
         return ret
 
 provider_classes = [AppleProvider]

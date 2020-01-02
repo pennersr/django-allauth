d1ca06ea803e832333731fa471589ebd4e767772
Vlad Doster <mvdoster@gmail.com>
2020-01-02 17:46:25 -0500

    fix(hubspot)
    Fixing tests to pass CI/CD pipeline

--- allauth/socialaccount/providers/hubspot/tests.py
+++ allauth/socialaccount/providers/hubspot/tests.py
@@ -9,14 +9,5 @@ class HubSpotTests(OAuth2TestsMixin, TestCase):
 
     def get_mocked_response(self):
         return MockedResponse(200, """
-{
-    "email": "user@example.com",
-}
-""")
-
-    def get_login_response_json(self, with_refresh_token=True):
-        return '{\
-    "refresh_token": "testrc",\
-    "access_token": "testac",\
-    "expires_in": "3600",\
-}'
+        {"username": "user@example.com",}
+        """)

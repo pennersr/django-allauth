abb80f45799f3d9b2142b3ccfd7b09b841aab689
Steve Recio <stevenrecio@gmail.com>
2020-01-18 16:19:35 -0500

    fix google provider

--- allauth/socialaccount/providers/google/provider.py
+++ allauth/socialaccount/providers/google/provider.py
@@ -40,7 +40,7 @@ class GoogleProvider(OAuth2Provider):
         return ret
 
     def extract_uid(self, data):
-        return str(data['id'])
+        return str(data['sub'])
 
     def extract_common_fields(self, data):
         return dict(email=data.get('email'),

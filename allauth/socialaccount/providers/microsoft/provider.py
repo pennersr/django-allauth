170e7b552fccfa90176a3c42339c370e8f639f1f
LunatikCoder <58997519+LunatikCoder@users.noreply.github.com>
2020-03-23 17:37:29 -0400

    fix(microsoft): 'NoneType' object has no attribute 'strip'

--- allauth/socialaccount/providers/microsoft/provider.py
+++ allauth/socialaccount/providers/microsoft/provider.py
@@ -8,7 +8,7 @@ class MicrosoftGraphAccount(ProviderAccount):
 
     def to_str(self):
         name = self.account.extra_data.get('displayName')
-        if name.strip() != '':
+        if name and name.strip() != '':
             return name
         return super(MicrosoftGraphAccount, self).to_str()
 

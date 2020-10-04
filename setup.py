c08937e4ebfe593f67fff5e806aaa0e2f5d80593
Raymond Penners <raymond.penners@intenct.nl>
2020-10-04 15:30:54 +0200

    chore(setup.py): Black

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from __future__ import print_function
+
 import io
 import os
 import sys
@@ -12,16 +13,26 @@ from setuptools import convert_path, find_packages, setup
 # of replicating them:
 standard_exclude = ["*.py", "*.pyc", "*~", ".*", "*.bak", "Makefile"]
 standard_exclude_directories = [
-    ".*", "CVS", "_darcs", "./build",
-    "./dist", "EGG-INFO", "*.egg-info",
-    "./example"
+    ".*",
+    "CVS",
+    "_darcs",
+    "./build",
+    "./dist",
+    "EGG-INFO",
+    "*.egg-info",
+    "./example",
 ]
 
 
 # Copied from paste/util/finddata.py
-def find_package_data(where=".", package="", exclude=standard_exclude,
-                      exclude_directories=standard_exclude_directories,
-                      only_in_packages=True, show_ignored=False):
+def find_package_data(
+    where=".",
+    package="",
+    exclude=standard_exclude,
+    exclude_directories=standard_exclude_directories,
+    only_in_packages=True,
+    show_ignored=False,
+):
     """
     Return a dictionary suitable for use in ``package_data``
     in a distutils ``setup.py`` file.
@@ -58,41 +69,39 @@ def find_package_data(where=".", package="", exclude=standard_exclude,
             if os.path.isdir(fn):
                 bad_name = False
                 for pattern in exclude_directories:
-                    if (fnmatchcase(name, pattern)
-                            or fn.lower() == pattern.lower()):
+                    if fnmatchcase(name, pattern) or fn.lower() == pattern.lower():
                         bad_name = True
                         if show_ignored:
                             print(
-                                "Directory %s ignored by pattern %s"
-                                % (fn, pattern), file=sys.stderr)
+                                "Directory %s ignored by pattern %s" % (fn, pattern),
+                                file=sys.stderr,
+                            )
                         break
                 if bad_name:
                     continue
-                if (os.path.isfile(os.path.join(fn, "__init__.py"))
-                        and not prefix):
+                if os.path.isfile(os.path.join(fn, "__init__.py")) and not prefix:
                     if not package:
                         new_package = name
                     else:
                         new_package = package + "." + name
                     stack.append((fn, "", new_package, False))
                 else:
-                    stack.append((fn, prefix + name + "/", package,
-                                  only_in_packages))
+                    stack.append((fn, prefix + name + "/", package, only_in_packages))
             elif package or not only_in_packages:
                 # is a file
                 bad_name = False
                 for pattern in exclude:
-                    if (fnmatchcase(name, pattern)
-                            or fn.lower() == pattern.lower()):
+                    if fnmatchcase(name, pattern) or fn.lower() == pattern.lower():
                         bad_name = True
                         if show_ignored:
                             print(
-                                "File %s ignored by pattern %s"
-                                % (fn, pattern), file=sys.stderr)
+                                "File %s ignored by pattern %s" % (fn, pattern),
+                                file=sys.stderr,
+                            )
                         break
                 if bad_name:
                     continue
-                out.setdefault(package, []).append(prefix+name)
+                out.setdefault(package, []).append(prefix + name)
     return out
 
 
@@ -100,53 +109,54 @@ excluded_directories = standard_exclude_directories
 
 package_data = find_package_data(exclude_directories=excluded_directories)
 
-long_description = io.open('README.rst', encoding='utf-8').read()
+long_description = io.open("README.rst", encoding="utf-8").read()
 
 # Dynamically calculate the version based on allauth.VERSION.
-version = __import__('allauth').__version__
+version = __import__("allauth").__version__
 
 METADATA = dict(
-    name='django-allauth',
+    name="django-allauth",
     version=version,
-    author='Raymond Penners',
-    author_email='raymond.penners@intenct.nl',
-    description='Integrated set of Django applications addressing'
-    ' authentication, registration, account management as well as'
-    ' 3rd party (social) account authentication.',
+    author="Raymond Penners",
+    author_email="raymond.penners@intenct.nl",
+    description="Integrated set of Django applications addressing"
+    " authentication, registration, account management as well as"
+    " 3rd party (social) account authentication.",
     long_description=long_description,
-    url='http://github.com/pennersr/django-allauth',
-    keywords='django auth account social openid twitter facebook oauth'
-    ' registration',
+    url="http://github.com/pennersr/django-allauth",
+    keywords="django auth account social openid twitter facebook oauth" " registration",
     tests_require=[],
-    install_requires=['Django >= 2.0',
-                      'python3-openid >= 3.0.8',
-                      'requests-oauthlib >= 0.3.0',
-                      'requests',
-                      'pyjwt[crypto] >=1.7'],
+    install_requires=[
+        "Django >= 2.0",
+        "python3-openid >= 3.0.8",
+        "requests-oauthlib >= 0.3.0",
+        "requests",
+        "pyjwt[crypto] >= 1.7",
+    ],
     include_package_data=True,
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Environment :: Web Environment',
-        'Topic :: Internet',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Framework :: Django',
-        'Framework :: Django :: 2.0',
-        'Framework :: Django :: 2.1',
-        'Framework :: Django :: 2.2',
-        'Framework :: Django :: 3.0',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Environment :: Web Environment",
+        "Topic :: Internet",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Framework :: Django",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
     ],
-    packages=find_packages(exclude=['example']),
+    packages=find_packages(exclude=["example"]),
     package_data=package_data,
 )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup(**METADATA)

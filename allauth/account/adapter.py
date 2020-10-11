4abf3f8ba51ca114743a141b51f54a0360189128
Raymond Penners <raymond.penners@intenct.nl>
2020-10-11 17:35:21 +0200

    feat(account/adapter): Support to-list in render_mail()

--- allauth/account/adapter.py
+++ allauth/account/adapter.py
@@ -101,6 +101,7 @@ class DefaultAccountAdapter(object):
         Renders an e-mail to `email`.  `template_prefix` identifies the
         e-mail that is to be sent, e.g. "account/email/email_confirmation"
         """
+        to = [email] if isinstance(email, str) else email
         subject = render_to_string("{0}_subject.txt".format(template_prefix), context)
         # remove superfluous line breaks
         subject = " ".join(subject.splitlines()).strip()
@@ -122,11 +123,11 @@ class DefaultAccountAdapter(object):
                     # We need at least one body
                     raise
         if "txt" in bodies:
-            msg = EmailMultiAlternatives(subject, bodies["txt"], from_email, [email])
+            msg = EmailMultiAlternatives(subject, bodies["txt"], from_email, to)
             if "html" in bodies:
                 msg.attach_alternative(bodies["html"], "text/html")
         else:
-            msg = EmailMessage(subject, bodies["html"], from_email, [email])
+            msg = EmailMessage(subject, bodies["html"], from_email, to)
             msg.content_subtype = "html"  # Main content is now text/html
         return msg
 

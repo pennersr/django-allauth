886cf628ecbb1c437a1d2964fad6190ed4f665b6
snowman2 <alansnow21@gmail.com>
2021-01-06 08:53:14 -0600

    fix(microsoft): Raise OAuth2Error when user id not found

--- allauth/socialaccount/providers/microsoft/tests.py
+++ allauth/socialaccount/providers/microsoft/tests.py
@@ -1,7 +1,11 @@
+import json
+
+from allauth.socialaccount.providers.oauth2.client import OAuth2Error
 from allauth.socialaccount.tests import OAuth2TestsMixin
 from allauth.tests import MockedResponse, TestCase
 
 from .provider import MicrosoftGraphProvider
+from .views import _check_errors
 
 
 class MicrosoftGraphTests(OAuth2TestsMixin, TestCase):
@@ -25,3 +29,28 @@ class MicrosoftGraphTests(OAuth2TestsMixin, TestCase):
         }
         """  # noqa
         return MockedResponse(200, response_data)
+
+    def test_invalid_data(self):
+        response = MockedResponse(200, json.dumps({}))
+        with self.assertRaises(OAuth2Error):
+            # No id, raises
+            _check_errors(response)
+
+    def test_profile_invalid_response(self):
+        data = {
+            "error": {
+                "code": "InvalidAuthenticationToken",
+                "message": "Access token validation failure. Invalid audience.",
+            }
+        }
+        response = MockedResponse(401, json.dumps(data))
+
+        with self.assertRaises(OAuth2Error):
+            # no id, 4xx code, raises with message
+            _check_errors(response)
+
+    def test_invalid_response(self):
+        response = MockedResponse(200, "invalid json data")
+        with self.assertRaises(OAuth2Error):
+            # bad json, raises
+            _check_errors(response)

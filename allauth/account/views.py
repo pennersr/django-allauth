b2b44aaeb6b0df20acf327adaf19089f0eb1bbdc
9mido <60831272+9mido@users.noreply.github.com>
2021-02-16 10:13:03 -0500

    Update views.py

--- allauth/account/views.py
+++ allauth/account/views.py
@@ -55,26 +55,27 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 
 def delete_account(request):
-    if request.user.is_anonymous:
-        return HttpResponseRedirect(app_settings.DELETE_REDIRECT_URL)
-    if request.user.is_authenticated:
-        if request.method == 'POST':
-            form = DeleteAccount(request.POST)
+    if app_settings.DELETE is True:
+        if request.user.is_anonymous:
+            return HttpResponseRedirect(app_settings.DELETE_REDIRECT_URL)
+        if request.user.is_authenticated:
+            if request.method == 'POST':
+                form = DeleteAccount(request.POST)
 
-            if form.is_valid():
-                if request.POST["delete_checkbox"]:
-                    account = User.objects.get(username=request.user)
-                    if account is not None:
-                        account.delete()
-                        logout(request)
-                        messages.info(request, "Your account has been deleted.")
-                        return HttpResponseRedirect(app_settings.DELETE_REDIRECT_URL)
-                    else:
-                        messages.error(request, "There was an error.")
-        else:
-            form = DeleteAccount()
-        context = {'form': form}
-        return render(request, 'account/delete.html', context)
+                if form.is_valid():
+                    if request.POST["delete_checkbox"]:
+                        account = User.objects.get(username=request.user)
+                        if account is not None:
+                            account.delete()
+                            logout(request)
+                            messages.info(request, "Your account has been deleted.")
+                            return HttpResponseRedirect(app_settings.DELETE_REDIRECT_URL)
+                        else:
+                            messages.error(request, "There was an error.")
+            else:
+                form = DeleteAccount()
+            context = {'form': form}
+            return render(request, 'account/delete.html', context)
 
 def _ajax_response(request, response, form=None, data=None):
     adapter = get_adapter(request)

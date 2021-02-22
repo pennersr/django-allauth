3e8ac98655125061ebc88ffed974cad94ee1bcfb
django-doctor <72647856+django-doctor@users.noreply.github.com>
2021-02-22 19:12:09 +0000

    chore: Replaced occurences of .count() by .exists()
    Co-authored-by: Django Doctor <help@django.doctor>

--- allauth/socialaccount/adapter.py
+++ allauth/socialaccount/adapter.py
@@ -131,15 +131,8 @@ class DefaultSocialAccountAdapter(object):
                 raise ValidationError(_("Your account has no password set" " up."))
             # No email address, no password reset
             if app_settings.EMAIL_VERIFICATION == EmailVerificationMethod.MANDATORY:
-                if (
-                    EmailAddress.objects.filter(
-                        user=account.user, verified=True
-                    ).count()
-                    == 0
-                ):
-                    raise ValidationError(
-                        _("Your account has no verified" " e-mail address.")
-                    )
+                if not EmailAddress.objects.filter(user=account.user, verified=True).exists():
+                    raise ValidationError(_("Your account has no verified e-mail address."))
 
     def is_auto_signup_allowed(self, request, sociallogin):
         # If email is specified, check for duplicate and if so, no auto signup.

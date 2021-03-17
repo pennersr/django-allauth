c52caa9ce8fb553e4dd2df499b66678e21b40efe
Alexandr Artemyev <mogost@gmail.com>
2021-03-18 00:04:15 +0300

    fix(django): default_app_config is deprecated in django 3.2
    https://docs.djangoproject.com/en/3.2/releases/3.2/#automatic-appconfig-discovery
    Fix RemovedInDjango41Warning

--- allauth/account/__init__.py
+++ allauth/account/__init__.py
@@ -1,1 +1,4 @@
-default_app_config = "allauth.account.apps.AccountConfig"
+import django
+
+if django.VERSION < (3, 2):  # pragma: no cover
+    default_app_config = "allauth.account.apps.AccountConfig"

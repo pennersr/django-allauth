308a1b13a8c7bf783be2723d9d799a2630c60000
Dawid Kosiński <tofik1432@gmail.com>
2021-03-23 21:57:23 +0100

    chore: Python 3.9 support

--- setup.py
+++ setup.py
@@ -148,6 +148,7 @@ METADATA = dict(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Framework :: Django",
         "Framework :: Django :: 2.0",
         "Framework :: Django :: 2.1",

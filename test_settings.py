7a1d1bc7dc7fbdf3a6f9f1f1180c7b139545892a
Glowsome <administrator@comsolve.nl>
2021-03-30 23:34:17 +0200

    feat(socialprovider) Add NetIQ AccessManager SocialProvider
    Adding SocialProvider NetIQ AccessManager

--- test_settings.py
+++ test_settings.py
@@ -108,6 +108,7 @@ INSTALLED_APPS = (
     "allauth.socialaccount.providers.meetup",
     "allauth.socialaccount.providers.microsoft",
     "allauth.socialaccount.providers.naver",
+    "allauth.socialaccount.providers.netiq",
     "allauth.socialaccount.providers.nextcloud",
     "allauth.socialaccount.providers.odnoklassniki",
     "allauth.socialaccount.providers.openid",

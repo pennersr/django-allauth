80e07a25803baea4e603251254c7d07ef2ad5bb5
Sumanth Ratna <sumanthratna@gmail.com>
2021-04-20 16:30:41 -0400

    feat(admin): Add `make_verified` action

--- allauth/account/admin.py
+++ allauth/account/admin.py
@@ -10,10 +10,16 @@ class EmailAddressAdmin(admin.ModelAdmin):
     list_filter = ("primary", "verified")
     search_fields = []
     raw_id_fields = ("user",)
+    actions = ["make_verified"]
 
     def get_search_fields(self, request):
         base_fields = get_adapter(request).get_user_search_fields()
         return ["email"] + list(map(lambda a: "user__" + a, base_fields))
+
+    def make_verified(self, request, queryset):
+        queryset.update(verified=True)
+
+    make_verified.short_description = "Mark selected email addresses as verified"
 
 
 class EmailConfirmationAdmin(admin.ModelAdmin):

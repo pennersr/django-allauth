75c39c10d20740c8607e2e92b7ea496c21c374ae
MohaMMad <49785234+mohammad2goodarzi@users.noreply.github.com>
2021-08-31 11:44:47 +0430

    add id field to social account models

--- allauth/socialaccount/apps.py
+++ allauth/socialaccount/apps.py
@@ -3,5 +3,6 @@ from django.utils.translation import gettext_lazy as _
 
 
 class SocialAccountConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'    
     name = "allauth.socialaccount"
     verbose_name = _("Social Accounts")

4c72590e5fe5f31caea1b99f1c2cd40cfa89294a
MohaMMad <49785234+mohammad2goodarzi@users.noreply.github.com>
2021-08-31 11:43:54 +0430

    add id field to account models

--- allauth/account/apps.py
+++ allauth/account/apps.py
@@ -3,5 +3,6 @@ from django.utils.translation import gettext_lazy as _
 
 
 class AccountConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = "allauth.account"
     verbose_name = _("Accounts")

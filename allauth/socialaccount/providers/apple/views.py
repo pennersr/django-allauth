7e9d36d0d5f26f515f962edfab06a19284fb0e57
David Runge <dave@sleepmap.de>
2021-11-24 15:48:38 +0100

    chore(apple): Remove use of deprecated pyjwt parameter
    allauth/socialaccount/providers/apple/views.py:
    Remove the use of `verify=True` when calling `jwt.decode()`, as it is
    deprecated with pyjwt == 1.7.0 (see
    https://github.com/jpadilla/pyjwt/blob/3f735fdf68cc865a6241aae1fcf92a410c73fbdb/jwt/api_jws.py#L132-L134)
    and removed in pywjt == 2.0.0.
    The default is now to use an (optional) `options` dict, that always uses
    `{"verify_signature": True}` as fallback if `options` is `None` or if
    `verify_signature` is not set in it.
    
    Closes #2977

--- allauth/socialaccount/providers/apple/views.py
+++ allauth/socialaccount/providers/apple/views.py
@@ -68,7 +68,6 @@ class AppleOAuth2Adapter(OAuth2Adapter):
                 id_token,
                 public_key,
                 algorithms=["RS256"],
-                verify=True,
                 audience=allowed_auds,
                 issuer="https://appleid.apple.com",
             )

015e7a2de4de4cc58ece3bba44e9c728ec38d59a
Darren Mckeeman <darren.mckeeman@gmail.com>
2021-12-04 11:21:58 -0800

    initial commit

---
+++ allauth/socialaccount/providers/metamask/views.py
@@ -0,0 +1,40 @@
+import requests
+
+from django.core.exceptions import ImproperlyConfigured
+
+from allauth.socialaccount import app_settings, providers
+from allauth.socialaccount.helpers import (
+    complete_social_login,
+    render_authentication_error,
+)
+from allauth.socialaccount.models import SocialLogin
+
+from .provider import MetamaskProvider
+
+
+def metamask_login(request):
+    assertion = request.POST.get("assertion", "")
+    settings = app_settings.PROVIDERS.get(MetamaskProvider.id, {})
+
+    resp = requests.post(
+        "https://verifier.login.persona.org/verify",
+        {"assertion": assertion, "audience": audience},
+    )
+    try:
+        resp.raise_for_status()
+        extra_data = resp.json()
+        if extra_data["status"] != "okay":
+            return render_authentication_error(
+                request,
+                provider_id=MetamaskProvider.id,
+                extra_context={"response": extra_data},
+            )
+    except (ValueError, requests.RequestException) as e:
+        return render_authentication_error(
+            request, provider_id=MetamaskProvider.id, exception=e
+        )
+    login = providers.registry.by_id(
+        MetamaskProvider.id, request
+    ).sociallogin_from_response(request, extra_data)
+    login.state = SocialLogin.state_from_request(request)
+    return complete_social_login(request, login)

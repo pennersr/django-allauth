0e4dbeded465f63bb873949f6ba6b9b71cd95665
Darren Mckeeman <darren.mckeeman@gmail.com>
2021-12-04 11:21:58 -0800

    initial commit

---
+++ allauth/socialaccount/providers/metamask/provider.py
@@ -0,0 +1,42 @@
+import json
+
+from django.template.loader import render_to_string
+from django.utils.html import escapejs
+
+from allauth.account.models import EmailAddress
+from allauth.socialaccount.providers.base import Provider, ProviderAccount
+
+
+class MetamaskAccount(ProviderAccount):
+    def to_str(self):
+        return self.account.uid
+
+
+class MetamaskProvider(Provider):
+    id = "metamask"
+    name = "Metamask"
+    account_class = MetamaskAccount
+
+    def media_js(self, request):
+        settings = self.get_settings()
+        request_parameters = settings.get("REQUEST_PARAMETERS", {})
+        ctx = {"request_parameters": json.dumps(request_parameters)}
+        return render_to_string("metamask/auth.html", ctx, request=request)
+
+    def get_login_url(self, request, **kwargs):
+        next_url = "'%s'" % escapejs(kwargs.get("next") or "")
+        process = "'%s'" % escapejs(kwargs.get("process") or "login")
+        return "javascript:allauth.metamask.login(%s, %s)" % (next_url, process)
+
+    def extract_uid(self, data):
+        return data["email"]
+
+    def extract_common_fields(self, data):
+        return dict(email=data["email"])
+
+    def extract_email_addresses(self, data):
+        ret = [EmailAddress(email=data["email"], verified=True, primary=True)]
+        return ret
+
+
+provider_classes = [MetamaskProvider]

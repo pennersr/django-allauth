285446043c4badb8756af76cba476c1fa0e37f6b
Darren Mckeeman <darren.mckeeman@gmail.com>
2021-12-05 15:56:50 -0800

    onboarding

--- allauth/socialaccount/providers/metamask/urls.py
+++ allauth/socialaccount/providers/metamask/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("metamask/login/", views.metamask_login, name="metamask_login")]
+urlpatterns = [
+    path("metamask/login/", views.metamask_login, name="metamask_login"),
+    path("metamask/nonce/", views.metamask_nonce, name="metamask_nonce"),
+]

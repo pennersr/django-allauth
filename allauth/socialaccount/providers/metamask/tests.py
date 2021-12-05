848a77ca3cd368f221699458711175c81723d8bd
Darren Mckeeman <darren.mckeeman@gmail.com>
2021-12-04 17:56:10 -0800

    unit tests.py

--- allauth/socialaccount/providers/metamask/tests.py
+++ allauth/socialaccount/providers/metamask/tests.py
@@ -1,12 +1,10 @@
-from django.test.utils import override_settings
-from django.urls import reverse
+from __future__ import unicode_literals
 
-from allauth.tests import TestCase, patch
 from allauth.socialaccount.tests import OAuthTestsMixin
-from allauth.utils import get_user_model
+from allauth.tests import TestCase
+
 from .provider import MetamaskProvider
 
-SOCIALACCOUNT_PROVIDERS = {"metamask": {"chainid": "6969"}}
 
 class MetamaskTests(OAuthTestsMixin, TestCase):
     provider_id = MetamaskProvider.id

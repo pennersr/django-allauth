a335cfa1d99e6390ea15c87eede5542557e77dca
Darren Mckeeman <darren.mckeeman@gmail.com>
2021-12-04 18:43:20 -0800

    unit tests.py

--- allauth/socialaccount/providers/metamask/tests.py
+++ allauth/socialaccount/providers/metamask/tests.py
@@ -10,9 +10,19 @@ from .provider import MetamaskProvider
 SOCIALACCOUNT_PROVIDERS = {"metamask": {"ChainID": "6969"}}
 
 
-class MetamaskTests(OAuthTestsMixin, TestCase):
-    provider_id = MetamaskProvider.id
+class MetamaskTests(TestCase):
     @override_settings(SOCIALACCOUNT_PROVIDERS=SOCIALACCOUNT_PROVIDERS)
     def test_login(self):
-        warnings.warn("Cannot test provider %s, no oauth mock" % self.provider.id)
-        return
+        with patch(
+            "allauth.socialaccount.providers.persona.views.requests"
+        ) as requests_mock:
+            requests_mock.post.return_value.json.return_value = {
+                "status": "okay",
+                "account": "0xfbfa21e9931f647bd6cc5be9e1a0dd9a41da535e",
+            }
+
+            resp = self.client.post(reverse("metamask_login"), dict(assertion="dummy"))
+            self.assertRedirects(
+                resp, "/accounts/profile/", fetch_redirect_response=False
+            )
+            get_user_model().objects.get(username="0xfbfa21e9931f647bd6cc5be9e1a0dd9a41da535e")

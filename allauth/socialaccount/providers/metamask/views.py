beee4f08df4651c84d392cee800f4bab84508037
Darren Mckeeman <darren.mckeeman@gmail.com>
2021-12-04 16:17:42 -0800

    changed provider added extract_uid

--- allauth/socialaccount/providers/metamask/views.py
+++ allauth/socialaccount/providers/metamask/views.py
@@ -15,7 +15,7 @@ from .provider import MetamaskProvider
 def metamask_login(request):
     accounts = request.POST.get("accounts", "")
     extra_data = json.dumps(accounts)
-    request.account = account
+    request.account = accounts
     request.uid = request.POST.get("account","")
     request.settings = app_settings.PROVIDERS.get(MetamaskProvider.id, {})
     login = providers.registry.by_id(

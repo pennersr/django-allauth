baae4afa8514cea4a3d09536a9acae3f0b3d78d3
Darren Mckeeman <darren.mckeeman@gmail.com>
2021-12-05 18:23:19 -0800

    onboarding

--- allauth/socialaccount/providers/metamask/urls.py
+++ allauth/socialaccount/providers/metamask/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("metamask/login/", views.metamask_login, name="metamask_login"),
     path("metamask/nonce/", views.metamask_nonce, name="metamask_nonce"),
+    path("metamask/login_api/", views.login_api, name="login_api"),
+
 ]
--- allauth/socialaccount/providers/metamask/views.py
+++ allauth/socialaccount/providers/metamask/views.py
@@ -35,3 +35,32 @@ def metamask_login(request):
     ).sociallogin_from_response(request, extra_data)
     login.state = SocialLogin.state_from_request(request)
     return complete_social_login(request, login)
+
+@require_http_methods(["GET", "POST"])
+def login_api(request):
+    if request.method == 'GET':
+        token = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for i in range(32))
+        request.session['login_token'] = token
+        return JsonResponse({'data': token, 'success': True})
+    else:
+        token = request.session.get('login_token')
+        if not token:
+            return JsonResponse({'error': _(
+                "No login token in session, please request token again by sending GET request to this url"),
+                'success': False})
+        else:
+            form = LoginForm(token, request.POST)
+            if form.is_valid():
+                signature, address = form.cleaned_data.get("signature"), form.cleaned_data.get("address")
+                del request.session['login_token']
+                user = authenticate(request, token=token, address=address, signature=signature)
+                if user:
+                    login(request, user, 'web3auth.backend.Web3Backend')
+
+                    return JsonResponse({'success': True, 'redirect_url': get_redirect_url(request)})
+                else:
+                    error = _("Can't find a user for the provided signature with address {address}").format(
+                        address=address)
+                    return JsonResponse({'success': False, 'error': error})
+            else:
+                return JsonResponse({'success': False, 'error': json.loads(form.errors.as_json())})

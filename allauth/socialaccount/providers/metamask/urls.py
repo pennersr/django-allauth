341aec4bad4cd80fac4a4452ce472c1dd10815a3
Darren Mckeeman <darren.mckeeman@gmail.com>
2021-12-07 17:07:03 -0800

    login get token portion

--- allauth/socialaccount/providers/metamask/urls.py
+++ allauth/socialaccount/providers/metamask/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import login_api
 
-urlpatterns = [
-    path("metamask/login_api/", login_api, name="login_api"),
-]
+
+urlpatterns = [path("metamask/login/", views.login_url, name="metamask_login")]

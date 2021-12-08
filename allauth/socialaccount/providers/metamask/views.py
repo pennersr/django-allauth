7ac69d231df508150034143f4b1b72d1ae899174
Darren Mckeeman <darren.mckeeman@gmail.com>
2021-12-08 07:50:55 -0800

    login get token portion

--- allauth/socialaccount/providers/metamask/views.py
+++ allauth/socialaccount/providers/metamask/views.py
@@ -26,7 +26,6 @@ from django import forms
 from django.utils.translation import ugettext_lazy as _
 from ethereum.utils import ecrecover_to_pub
 
-
 def sig_to_vrs(sig):
     #    sig_bytes = bytes.fromhex(sig[2:])
     r = int(sig[2:66], 16)
@@ -34,17 +33,14 @@ def sig_to_vrs(sig):
     v = int(sig[130:], 16)
     return v, r, s
 
-
 def hash_personal_message(msg):
     padded = "\x19Ethereum Signed Message:\n" + str(len(msg)) + msg
     return sha3.keccak_256(bytes(padded, 'utf8')).digest()
-
 
 def recover_to_addr(msg, sig):
     msghash = hash_personal_message(msg)
     vrs = sig_to_vrs(sig)
     return '0x' + sha3.keccak_256(ecrecover_to_pub(msghash, *vrs)).hexdigest()[24:]
-
 
 def validate_eth_address(value):
     if not is_hex_address(value):

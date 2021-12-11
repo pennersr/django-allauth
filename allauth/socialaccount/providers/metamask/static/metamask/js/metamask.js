427166d5cc5e7cc8a82e1babd8337814c13e1683
Darren Mckeeman <darren.mckeeman@gmail.com>
2021-12-11 15:50:38 -0800

    error handling improved

--- allauth/socialaccount/providers/metamask/static/metamask/js/metamask.js
+++ allauth/socialaccount/providers/metamask/static/metamask/js/metamask.js
@@ -27,6 +27,20 @@ async function loginwithtoken(token,account,next,process){
             process: 'verify',
         })
 })
+  .then(function (response) {
+	// The API call was successful!
+	return response.text();
+}).then(function (html) {
+
+	// Convert the HTML string into a document object
+	var parser = new DOMParser();
+	var doc = parser.parseFromString(html, 'text/html');
+	document.querySelector('html').innerHTML = doc
+
+}).catch(function (err) {
+	// There was an error
+	console.warn('Something went wrong.', err);
+});
   // window.location.href = next;
 }
 

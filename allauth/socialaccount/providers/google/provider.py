883aef0188ee2dff68c7738bd721c98377bafbe1
Vipul Kushwaha <71925064+Vipulkushwaha@users.noreply.github.com>
2022-04-05 18:01:03 +0530

    Check for 'sub' if 'id' is not present in 'data'
    When ID Token is used, in place of an access token, (as in the case of an android app), a 'sub' key is present instead of 'id' in 'data'. This change considers both keys.

--- allauth/socialaccount/providers/google/provider.py
+++ allauth/socialaccount/providers/google/provider.py
@@ -39,7 +39,10 @@ class GoogleProvider(OAuth2Provider):
         return ret
 
     def extract_uid(self, data):
-        return str(data["id"])
+        try:
+            return str(data["id"])
+        except:
+            return str(data["sub"])
 
     def extract_common_fields(self, data):
         return dict(

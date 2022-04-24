aa5c730460bd7578dc19a34537b9b14fdc7bf08c
Moncef ABBOUD <moncef.abboud95@gmail.com>
2022-04-24 12:45:39 +0200

    feat(providers): add Hubspot

---
+++ allauth/socialaccount/providers/hubspot/tests.py
@@ -0,0 +1,28 @@
+from allauth.socialaccount.tests import OAuth2TestsMixin
+from allauth.tests import MockedResponse, TestCase
+
+from .provider import HubspotProvider
+
+
+class HubspotTests(OAuth2TestsMixin, TestCase):
+
+    provider_id = HubspotProvider.id
+
+    def get_mocked_response(self):
+        return MockedResponse(
+            200,
+            """{
+                    "token": "CNye4dqFMBICAAEYhOKlDZZ_z6IVKI_xMjIUgmFsNQzgBjNE9YBmhAhNOtfN0ak6BAAAAEFCFIIwn2EVRLpvJI9hP4tbIeKHw7ZXSgNldTFSAFoA",
+                    "user": "m@acme.com",
+                    "hub_domain": "acme.com",
+                    "scopes": ["oauth"],
+                    "scope_to_scope_group_pks": [25, 31],
+                    "trial_scopes": [],
+                    "trial_scope_to_scope_group_pks": [],
+                    "hub_id": 211580,
+                    "app_id": 833572,
+                    "expires_in": 1799,
+                    "user_id": 42607123,
+                    "token_type": "access"
+                }""",
+        )
--- docs/providers.rst
+++ docs/providers.rst
@@ -1024,6 +1024,18 @@ Development callback URL
     http://localhost:8000/accounts/instagram/login/callback/
 
 
+Hubspot
+--------
+
+App registration (get your key and secret here)
+    https://developers.hubspot.com/docs/api/creating-an-app
+
+Authentication documentation
+    https://developers.hubspot.com/docs/api/working-with-oauth
+
+Development callback URL
+    https://localhost:8000/accounts/hubspot/login/callback/
+
 Instagram
 ---------
 

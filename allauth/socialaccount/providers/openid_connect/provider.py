e1acb766f5bc578009ceb4db414f5730ce02adab
Christian Assing <cassing@redhat.com>
2023-01-24 08:01:09 +0100

    fix(openid-connect): add missing email to common fields
    only common fields are used to populate user attributes and email was missing
    
    Closes #3241

--- allauth/socialaccount/providers/openid_connect/provider.py
+++ allauth/socialaccount/providers/openid_connect/provider.py
@@ -42,6 +42,7 @@ class OpenIDConnectProvider(OAuth2Provider):
 
     def extract_common_fields(self, data):
         return dict(
+            email=data.get("email"),
             username=data.get("preferred_username"),
             name=data.get("name"),
             user_id=data.get("user_id"),

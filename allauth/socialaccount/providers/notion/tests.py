371b1cc8c25d01733966b846722d8df9afc54cf8
Felipe Faria <2829082+synchronizing@users.noreply.github.com>
2023-05-12 00:41:38 -0400

    Fixed tests.

--- allauth/socialaccount/providers/notion/tests.py
+++ allauth/socialaccount/providers/notion/tests.py
@@ -23,4 +23,31 @@ class NotionTests(OAuth2TestsMixin, TestCase):
                 },
             }
             """,
-        )
+        )  # noqa
+
+    def get_login_response_json(self, with_refresh_token=False):
+        """
+        Docs here:
+        https://developers.notion.com/docs/authorization#step-4-notion-responds-with-an-access_token-and-additional-information
+        """
+        return """
+        {
+            "access_token": "test123",
+            "bot_id": "bot-abc",
+            "duplicated_template_id": "template-abc",
+            "owner": {
+            "workspace_id": "workspace-abc",
+                "user": {
+                    "id": "test123",
+                    "name": "John Doe",
+                    "avatar_url": "",
+                    "person": {
+                        "email": "john@example.com"
+                    }
+                }
+            },
+            "workspace_icon": "https://example.com/icon.png",
+            "workspace_id": "workspace-abc",
+            "workspace_name": "My Workspace"
+        }
+        """

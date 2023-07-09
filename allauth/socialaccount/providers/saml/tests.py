752b888c92494ecd429cd2d40233a1c7c6d45132
Raymond Penners <raymond.penners@intenct.nl>
2023-07-09 13:23:23 +0200

    tests(saml): Add coverage for SAML config builder

--- allauth/socialaccount/providers/saml/tests.py
+++ allauth/socialaccount/providers/saml/tests.py
@@ -3,8 +3,11 @@ from unittest.mock import patch
 from django.urls import reverse
 from django.utils.http import urlencode
 
+import pytest
+
 from allauth.account.models import EmailAddress
 from allauth.socialaccount.models import SocialAccount
+from allauth.socialaccount.providers.saml.utils import build_saml_config
 
 
 def test_acs(client, db, saml_settings, acs_saml_response, mocked_signature_validation):
@@ -61,3 +64,57 @@ def test_sls(auth_client, db, saml_settings, user_factory, sls_saml_request):
     assert resp["location"].startswith(
         "https://dev-123.us.auth0.com/samlp/456?SAMLResponse="
     )
+
+
+@pytest.mark.parametrize(
+    "provider_config",
+    [
+        {
+            "idp": {
+                "entity_id": "dummy",
+                "sso_url": "https://idp.org/sso/",
+                "slo_url": "https://idp.saml.org/slo/",
+                "x509cert": "cert",
+            }
+        },
+    ],
+)
+def test_build_saml_config_without_metadata_url(rf, provider_config):
+    request = rf.get("/")
+    config = build_saml_config(request, provider_config, "org")
+    assert config["idp"]["entityId"] == "dummy"
+    assert config["idp"]["x509cert"] == "cert"
+    assert config["idp"]["singleSignOnService"] == {"url": "https://idp.org/sso/"}
+    assert config["idp"]["singleLogoutService"] == {"url": "https://idp.saml.org/slo/"}
+
+
+@pytest.mark.parametrize(
+    "provider_config",
+    [
+        {
+            "idp": {
+                "entity_id": "dummy",
+                "metadata_url": "https://idp.org/sso/",
+            }
+        },
+    ],
+)
+def test_build_saml_config(rf, provider_config):
+    request = rf.get("/")
+    with patch(
+        "onelogin.saml2.idp_metadata_parser.OneLogin_Saml2_IdPMetadataParser.parse_remote"
+    ) as parse_mock:
+        parse_mock.return_value = {
+            "idp": {
+                "entityId": "dummy",
+                "singleSignOnService": {"url": "https://idp.org/sso/"},
+                "singleLogoutService": {"url": "https://idp.saml.org/slo/"},
+                "x509cert": "cert",
+            }
+        }
+        config = build_saml_config(request, provider_config, "org")
+
+    assert config["idp"]["entityId"] == "dummy"
+    assert config["idp"]["x509cert"] == "cert"
+    assert config["idp"]["singleSignOnService"] == {"url": "https://idp.org/sso/"}
+    assert config["idp"]["singleLogoutService"] == {"url": "https://idp.saml.org/slo/"}

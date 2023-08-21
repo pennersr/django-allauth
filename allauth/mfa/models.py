994900ce5d41bf59a7077bb9aae4f65ac1e234ff
Raymond Penners <raymond.penners@intenct.nl>
2023-08-21 20:36:36 +0200

    fix(mfa): Delete dangling recovery codes

--- allauth/mfa/models.py
+++ allauth/mfa/models.py
@@ -3,10 +3,19 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+class AuthenticatorManager(models.Manager):
+    def delete_dangling_recovery_codes(self, user):
+        qs = Authenticator.objects.filter(user=user)
+        if not qs.exclude(type=Authenticator.Type.RECOVERY_CODES).exists():
+            qs.delete()
+
+
 class Authenticator(models.Model):
     class Type(models.TextChoices):
         RECOVERY_CODES = "recovery_codes", _("Recovery codes")
         TOTP = "totp", _("TOTP Authenticator")
+
+    objects = AuthenticatorManager()
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     type = models.CharField(max_length=20, choices=Type.choices)

08046bc9cf3eccd372cf68a2002a0f12bb057b91
Raymond Penners <raymond.penners@intenct.nl>
2023-08-25 23:13:13 +0200

    feat(mfa): Support for migrated recovery codes

--- allauth/mfa/recovery_codes.py
+++ allauth/mfa/recovery_codes.py
@@ -31,7 +31,16 @@ class RecoveryCodes:
         instance.save()
         return cls(instance)
 
+    @classmethod
+    def generate_seed(self):
+        key = binascii.hexlify(os.urandom(20)).decode("ascii")
+        return key
+
     def generate_codes(self):
+        migrated_codes = self.instance.data.get("migrated_codes")
+        if migrated_codes is not None:
+            return migrated_codes
+
         ret = []
         seed = decrypt(self.instance.data["seed"])
         h = hmac.new(key=seed.encode("ascii"), msg=None, digestmod=sha1)
@@ -43,34 +52,48 @@ class RecoveryCodes:
             ret.append(fmt_value)
         return ret
 
-    def is_code_used(self, i):
+    def _is_code_used(self, i):
         used_mask = self.instance.data["used_mask"]
         return bool(used_mask & (1 << i))
 
-    def mark_code_used(self, i):
+    def _mark_code_used(self, i):
         used_mask = self.instance.data["used_mask"]
         used_mask |= 1 << i
         self.instance.data["used_mask"] = used_mask
         self.instance.save()
 
     def get_unused_codes(self):
+        migrated_codes = self.instance.data.get("migrated_codes")
+        if migrated_codes is not None:
+            return migrated_codes
+
         ret = []
         for i, code in enumerate(self.generate_codes()):
-            if self.is_code_used(i):
+            if self._is_code_used(i):
                 continue
             ret.append(code)
         return ret
 
-    @classmethod
-    def generate_seed(self):
-        key = binascii.hexlify(os.urandom(20)).decode("ascii")
-        return key
+    def _validate_migrated_code(self, code):
+        migrated_codes = self.instance.data.get("migrated_codes")
+        if migrated_codes is None:
+            return None
+        if code in migrated_codes:
+            migrated_codes.remove(code)
+            self.instance.data["migrated_codes"] = migrated_codes
+            self.instance.save()
+            return True
+        return False
 
     def validate_code(self, code):
+        ret = self._validate_migrated_code(code)
+        if ret is not None:
+            return ret
+
         for i, c in enumerate(self.generate_codes()):
-            if self.is_code_used(i):
+            if self._is_code_used(i):
                 continue
             if code == c:
-                self.mark_code_used(i)
+                self._mark_code_used(i)
                 return True
         return False

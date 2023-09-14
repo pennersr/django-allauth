c2461350cedb781656704e1256e6ffc75b198579
amirreza <amir.rsf1380@gmail.com>
2023-09-14 21:13:59 +0330

    black

--- test_settings.py
+++ test_settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = (
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-
     "allauth.account.middleware.AccountMiddleware",
 )
 

b7ce2f8852778f7949bb8f944ac03dcf1adb09fc
Raymond Penners <raymond.penners@intenct.nl>
2023-10-08 15:36:31 +0200

    docs: Change theme

--- docs/conf.py
+++ docs/conf.py
@@ -93,7 +93,7 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "default"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -259,6 +259,7 @@ texinfo_documents = [
 
 extensions = [
     "sphinx.ext.autodoc",
+    "sphinx_rtd_theme",
 ]
 
 sys.path.insert(0, os.path.abspath("../"))

333d5cebaa4921532fc611558202241f51d1282e
Dhanish Vijayan <dhanishvijayan@gmail.com>
2023-10-19 00:30:17 +0530

    Update 0001_initial.py for migration bug fix

--- allauth/socialaccount/migrations/0001_initial.py
+++ allauth/socialaccount/migrations/0001_initial.py
@@ -12,6 +12,7 @@ class Migration(migrations.Migration):
     dependencies = (
         [
             ("sites", "0001_initial"),
+            migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ]
         if app_settings.SITES_ENABLED
         else []

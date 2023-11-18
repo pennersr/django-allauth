c32d2acbdc6e135db12bf622bb28383099dad85e
varun kumar <varun.kumar@zenatix.com>
2023-11-18 17:15:51 +0530

    Add social accounts signals

--- allauth/socialaccount/adapter.py
+++ allauth/socialaccount/adapter.py
@@ -302,6 +302,9 @@ class DefaultSocialAccountAdapter(object):
         elif len(apps) == 0:
             raise SocialApp.DoesNotExist()
         return apps[0]
+        
+    def send_notification_mail(self, *args, **kwargs):
+        return get_account_adapter().send_notification_mail(*args, **kwargs)
 
     def get_requests_session(self):
         import requests

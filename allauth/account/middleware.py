95eb6783cf6485120f6a9bdc3eb47b7ab0535297
Raymond Penners <raymond.penners@intenct.nl>
2023-12-14 22:15:29 +0100

    fix(account/middleware): AccountMiddleware async capable

--- allauth/account/middleware.py
+++ allauth/account/middleware.py
@@ -1,38 +1,53 @@
 from django.conf import settings
+from django.utils.decorators import sync_and_async_middleware
+
+from asgiref.sync import iscoroutinefunction
 
 from allauth.core import context
 from allauth.core.exceptions import ImmediateHttpResponse
 
 
-class AccountMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-        # One-time configuration and initialization.
+@sync_and_async_middleware
+def AccountMiddleware(get_response):
+    if iscoroutinefunction(get_response):
 
-    def __call__(self, request):
-        with context.request_context(request):
-            response = self.get_response(request)
-            self._remove_dangling_login(request, response)
-            return response
+        async def middleware(request):
+            with context.request_context(request):
+                try:
+                    response = await get_response(request)
+                    _remove_dangling_login(request, response)
+                    return response
+                except ImmediateHttpResponse as e:
+                    return e.response
 
-    def process_exception(self, request, exception):
-        if isinstance(exception, ImmediateHttpResponse):
-            return exception.response
+    else:
 
-    def _remove_dangling_login(self, request, response):
-        content_type = response.headers.get("content-type")
-        if content_type:
-            content_type = content_type.partition(";")[0]
-        if content_type and content_type != "text/html":
-            return
-        if request.path.startswith(settings.STATIC_URL) or request.path in [
-            "/favicon.ico",
-            "/robots.txt",
-            "/humans.txt",
-        ]:
-            return
-        if response.status_code // 100 != 2:
-            return
-        if not getattr(request, "_account_login_accessed", False):
-            if "account_login" in request.session:
-                request.session.pop("account_login")
+        def middleware(request):
+            with context.request_context(request):
+                try:
+                    response = get_response(request)
+                    _remove_dangling_login(request, response)
+                    return response
+                except ImmediateHttpResponse as e:
+                    return e.response
+
+    return middleware
+
+
+def _remove_dangling_login(request, response):
+    content_type = response.headers.get("content-type")
+    if content_type:
+        content_type = content_type.partition(";")[0]
+    if content_type and content_type != "text/html":
+        return
+    if request.path.startswith(settings.STATIC_URL) or request.path in [
+        "/favicon.ico",
+        "/robots.txt",
+        "/humans.txt",
+    ]:
+        return
+    if response.status_code // 100 != 2:
+        return
+    if not getattr(request, "_account_login_accessed", False):
+        if "account_login" in request.session:
+            request.session.pop("account_login")

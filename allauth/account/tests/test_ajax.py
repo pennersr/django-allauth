f836f89b4f221e6928a645ca6ab2da22312dad74
Raymond Penners <raymond.penners@intenct.nl>
2024-01-08 20:52:36 +0100

    tests(account): ajax TestCase -> pytest

--- allauth/account/tests/test_ajax.py
+++ allauth/account/tests/test_ajax.py
@@ -1,81 +1,74 @@
 import json
 
 from django.conf import settings
-from django.contrib.auth import get_user_model
-from django.core import mail
-from django.test.utils import override_settings
 from django.urls import reverse
 
+import pytest
+from pytest_django.asserts import assertRedirects
+
 from allauth.account import app_settings
-from allauth.tests import TestCase
 
 
-class AjaxTests(TestCase):
-    def _send_post_request(self, **kwargs):
-        return self.client.post(
-            reverse("account_signup"),
-            {
-                "username": "johndoe",
-                "email": "john@example.org",
-                "email2": "john@example.org",
-                "password1": "johndoe",
-                "password2": "johndoe",
-            },
-            **kwargs,
-        )
-
-    def test_no_ajax_header(self):
-        resp = self._send_post_request()
-        self.assertEqual(302, resp.status_code)
-        self.assertRedirects(
+@pytest.mark.parametrize(
+    "headers,ajax_expected",
+    [
+        ({}, False),
+        ({"HTTP_X_REQUESTED_WITH": "XMLHttpRequest"}, True),
+        ({"HTTP_ACCEPT": "application/json"}, True),
+    ],
+)
+def test_ajax_headers(db, client, headers, ajax_expected):
+    resp = client.post(
+        reverse("account_signup"),
+        {
+            "username": "johndoe",
+            "email": "john@example.org",
+            "email2": "john@example.org",
+            "password1": "johndoe",
+            "password2": "johndoe",
+        },
+        **headers,
+    )
+    if ajax_expected:
+        assert resp.status_code == 200
+        assert resp.json()["location"] == settings.LOGIN_REDIRECT_URL
+        assert resp.json()["location"] == settings.LOGIN_REDIRECT_URL
+    else:
+        assert resp.status_code == 302
+        assertRedirects(
             resp, settings.LOGIN_REDIRECT_URL, fetch_redirect_response=False
         )
 
-    def test_ajax_header_x_requested_with(self):
-        resp = self._send_post_request(HTTP_X_REQUESTED_WITH="XMLHttpRequest")
-        self.assertEqual(200, resp.status_code)
-        self.assertEqual(settings.LOGIN_REDIRECT_URL, resp.json()["location"])
 
-    def test_ajax_header_http_accept(self):
-        resp = self._send_post_request(HTTP_ACCEPT="application/json")
-        self.assertEqual(200, resp.status_code)
-        self.assertEqual(settings.LOGIN_REDIRECT_URL, resp.json()["location"])
-
-    def test_ajax_password_reset(self):
-        get_user_model().objects.create(
-            username="john", email="john@example.org", is_active=True
-        )
-        resp = self.client.post(
-            reverse("account_reset_password"),
-            data={"email": "john@example.org"},
-            HTTP_X_REQUESTED_WITH="XMLHttpRequest",
-        )
-        self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(mail.outbox[0].to, ["john@example.org"])
-        self.assertEqual(resp["content-type"], "application/json")
-
-    def test_ajax_login_fail(self):
-        resp = self.client.post(
-            reverse("account_login"),
-            {},
-            HTTP_X_REQUESTED_WITH="XMLHttpRequest",
-        )
-        self.assertEqual(resp.status_code, 400)
-        json.loads(resp.content.decode("utf8"))
-        # TODO: Actually test something
-
-    @override_settings(
-        ACCOUNT_EMAIL_VERIFICATION=app_settings.EmailVerificationMethod.OPTIONAL
+def test_ajax_password_reset(client, user, mailoutbox):
+    resp = client.post(
+        reverse("account_reset_password"),
+        data={"email": user.email},
+        HTTP_X_REQUESTED_WITH="XMLHttpRequest",
     )
-    def test_ajax_login_success(self):
-        user = get_user_model().objects.create(username="john", is_active=True)
-        user.set_password("doe")
-        user.save()
-        resp = self.client.post(
-            reverse("account_login"),
-            {"login": "john", "password": "doe"},
-            HTTP_X_REQUESTED_WITH="XMLHttpRequest",
-        )
-        self.assertEqual(resp.status_code, 200)
-        data = json.loads(resp.content.decode("utf8"))
-        self.assertEqual(data["location"], "/accounts/profile/")
+    assert len(mailoutbox) == 1
+    assert mailoutbox[0].to == [user.email]
+    assert resp["content-type"] == "application/json"
+
+
+def test_ajax_login_fail(client, db):
+    resp = client.post(
+        reverse("account_login"),
+        {},
+        HTTP_X_REQUESTED_WITH="XMLHttpRequest",
+    )
+    assert resp.status_code == 400
+    json.loads(resp.content.decode("utf8"))
+    # TODO: Actually test something
+
+
+def test_ajax_login_success(settings, user, user_password, client):
+    settings.ACCOUNT_EMAIL_VERIFICATION = app_settings.EmailVerificationMethod.OPTIONAL
+    resp = client.post(
+        reverse("account_login"),
+        {"login": user.username, "password": user_password},
+        HTTP_X_REQUESTED_WITH="XMLHttpRequest",
+    )
+    assert resp.status_code == 200
+    data = json.loads(resp.content.decode("utf8"))
+    assert data["location"] == "/accounts/profile/"

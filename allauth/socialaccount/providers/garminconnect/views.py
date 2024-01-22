3998d80b56a23e928b7077d3d3e1edf25473170a
soh55 <ohss117@gmail.com>
2024-01-21 20:26:47 -0600

    Improve code formatting and reduce code duplication

--- allauth/socialaccount/providers/garminconnect/views.py
+++ allauth/socialaccount/providers/garminconnect/views.py
@@ -12,7 +12,7 @@ from .client import GarminConnectOAuthClient
 
 
 class GarminConnectAPI(OAuth):
-    url = 'https://apis.garmin.com/wellness-api/rest/user/id'
+    url = "https://apis.garmin.com/wellness-api/rest/user/id"
 
     def get_user_info(self):
         data = json.loads(self.query(self.url))
@@ -21,20 +21,25 @@ class GarminConnectAPI(OAuth):
 
 class GarminConnectOAuthAdapter(OAuthAdapter):
     provider_id = GarminConnectProvider.id
-    request_token_url = 'https://connectapi.garmin.com/oauth-service/oauth/request_token'
-    access_token_url = 'https://connectapi.garmin.com/oauth-service/oauth/access_token'
-    authorize_url = 'https://connect.garmin.com/oauthConfirm'
-    # profile_url = 'https://connectapi.garmin.com/user/id'
+    request_token_url = (
+        "https://connectapi.garmin.com/oauth-service/oauth/request_token"
+    )
+    access_token_url = "https://connectapi.garmin.com/oauth-service/oauth/access_token"
+    authorize_url = "https://connect.garmin.com/oauthConfirm"
 
     def complete_login(self, request, app, token, response):
-
-        client = GarminConnectAPI(request, app.client_id, app.secret, self.request_token_url)
+        client = GarminConnectAPI(
+            request, app.client_id, app.secret, self.request_token_url
+        )
         extra_data = client.get_user_info()
 
         return self.get_provider().sociallogin_from_response(request, extra_data)
 
 
-class GarminConnectLoginView(OAuthLoginView):
+class GetClientMixin(object):
+    """
+    Mixin handling common code for OAuthLoginView and OAuthCallbackView
+    """
     def _get_client(self, request, callback_url):
         provider = self.adapter.get_provider()
         app = provider.app
@@ -54,26 +59,14 @@ class GarminConnectLoginView(OAuthLoginView):
         )
         return client
 
-class GarminConnectOAuthCallbackView(OAuthCallbackView):
-    def _get_client(self, request, callback_url):
-        provider = self.adapter.get_provider()
-        app = provider.app
-        scope = " ".join(provider.get_scope(request))
-        parameters = {}
-        if scope:
-            parameters["scope"] = scope
-        client = GarminConnectOAuthClient(
-            request,
-            app.client_id,
-            app.secret,
-            self.adapter.request_token_url,
-            self.adapter.access_token_url,
-            callback_url,
-            parameters=parameters,
-            provider=provider,
-        )
-        return client
+
+class GarminConnectLoginView(GetClientMixin, OAuthLoginView):
+    pass
+
+
+class GarminConnectOAuthCallbackView(GetClientMixin, OAuthCallbackView):
+    pass
+
 
 oauth_login = GarminConnectLoginView.adapter_view(GarminConnectOAuthAdapter)
 oauth_callback = GarminConnectOAuthCallbackView.adapter_view(GarminConnectOAuthAdapter)
-

f977ed82a1fbf5fccf7817637aa22ae916b0be9c
soh55 <ohss117@gmail.com>
2024-01-21 20:06:10 -0600

    Add test details

--- allauth/socialaccount/providers/garminconnect/tests.py
+++ allauth/socialaccount/providers/garminconnect/tests.py
@@ -11,5 +11,6 @@ class GarminConnectTests(OAuthTestsMixin, TestCase):
     provider_id = GarminConnectProvider.id
 
     def get_mocked_response(self):
-        data = {"userId": 1234567890}
+        # Garmin only provides userId in the response
+        data = {"userId": "d3315b1072421d0dd7c8f6b8e1de4df8"}
         return [MockedResponse(200, json.dumps(data))]

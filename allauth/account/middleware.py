4a09c071e518f08d5ae4cb4008da1dfe5d3e7aae
Raymond Penners <raymond.penners@intenct.nl>
2024-01-22 22:08:48 +0100

    fix(account): Async session access

--- allauth/account/middleware.py
+++ allauth/account/middleware.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.utils.decorators import sync_and_async_middleware
 
-from asgiref.sync import iscoroutinefunction
+from asgiref.sync import iscoroutinefunction, sync_to_async
 
 from allauth.core import context
 from allauth.core.exceptions import ImmediateHttpResponse
@@ -15,7 +15,9 @@ def AccountMiddleware(get_response):
             with context.request_context(request):
                 try:
                     response = await get_response(request)
-                    _remove_dangling_login(request, response)
+                    _remove_dangling_login(
+                        request, response, sync_to_async(_session_check)
+                    )
                     return response
                 except ImmediateHttpResponse as e:
                     return e.response
@@ -26,7 +28,7 @@ def AccountMiddleware(get_response):
             with context.request_context(request):
                 try:
                     response = get_response(request)
-                    _remove_dangling_login(request, response)
+                    _remove_dangling_login(request, response, _session_check)
                     return response
                 except ImmediateHttpResponse as e:
                     return e.response
@@ -34,7 +36,7 @@ def AccountMiddleware(get_response):
     return middleware
 
 
-def _remove_dangling_login(request, response):
+def _remove_dangling_login(request, response, session_check):
     content_type = response.headers.get("content-type")
     if content_type:
         content_type = content_type.partition(";")[0]
@@ -48,6 +50,10 @@ def _remove_dangling_login(request, response):
         return
     if response.status_code // 100 != 2:
         return
+    session_check(request)
+
+
+def _session_check(request):
     if not getattr(request, "_account_login_accessed", False):
         if "account_login" in request.session:
             request.session.pop("account_login")

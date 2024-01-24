a6f1abb3d148457213bd7536ff099018d8a1d233
soh55 <ohss117@gmail.com>
2024-01-23 19:42:21 -0600

    Made suggested code improvements

--- allauth/socialaccount/providers/garminconnect/provider.py
+++ allauth/socialaccount/providers/garminconnect/provider.py
@@ -3,17 +3,6 @@ from allauth.socialaccount.providers.oauth.provider import OAuthProvider
 
 
 class GarminConnectAccount(ProviderAccount):
-    def get_profile_url(self):
-        """
-        Garmin Connect API doesn't provide a profile URL
-        """
-        return None
-
-    def get_avatar_url(self):
-        """
-        Garmin Connect API doesn't provide an avatar URL
-        """
-        return None
 
     def to_str(self):
         """
@@ -31,14 +20,14 @@ class GarminConnectProvider(OAuthProvider):
         return ['read', 'write']
 
     def extract_uid(self, data):
-        return str(data['userId'])
+        return data['userId']
 
     def extract_common_fields(self, data):
         # Garmin Connect API doesn't return any other data than userId
         return {}
 
     def sociallogin_from_response(self, request, response):
-        return super(GarminConnectProvider, self).sociallogin_from_response(request, response)
+        return super().sociallogin_from_response(request, response)
 
 
 provider_classes = [GarminConnectProvider]

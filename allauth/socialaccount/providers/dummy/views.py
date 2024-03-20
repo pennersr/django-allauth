ffdfb3032c2375821af9ca2d53b5d2d3e22bf1b7
Raymond Penners <raymond.penners@intenct.nl>
2024-03-20 16:38:42 +0100

    feast(providers/dummy): Cancel flow

--- allauth/socialaccount/providers/dummy/views.py
+++ allauth/socialaccount/providers/dummy/views.py
@@ -2,8 +2,12 @@ from django.views.generic.base import View
 from django.views.generic.edit import FormView
 
 from allauth.socialaccount.adapter import get_adapter
-from allauth.socialaccount.helpers import complete_social_login
+from allauth.socialaccount.helpers import (
+    complete_social_login,
+    render_authentication_error,
+)
 from allauth.socialaccount.models import SocialLogin
+from allauth.socialaccount.providers.base.constants import AuthError
 from allauth.socialaccount.providers.base.utils import respond_to_login_on_get
 from allauth.socialaccount.providers.dummy.forms import AuthenticateForm
 from allauth.socialaccount.providers.dummy.provider import DummyProvider
@@ -25,9 +29,17 @@ class AuthenticateView(FormView):
     form_class = AuthenticateForm
     template_name = "dummy/authenticate_form.html"
 
+    def dispatch(self, request, *args, **kwargs):
+        self.provider = get_adapter().get_provider(self.request, DummyProvider.id)
+        if request.method == "POST" and request.POST.get("action") == "cancel":
+            return render_authentication_error(
+                request, self.provider, error=AuthError.CANCELLED
+            )
+
+        return super().dispatch(request, *args, **kwargs)
+
     def form_valid(self, form):
-        provider = get_adapter().get_provider(self.request, DummyProvider.id)
-        login = provider.sociallogin_from_response(self.request, form.cleaned_data)
+        login = self.provider.sociallogin_from_response(self.request, form.cleaned_data)
         login.state = SocialLogin.unstash_state(self.request)
         return complete_social_login(self.request, login)
 

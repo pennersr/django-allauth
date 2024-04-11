109ac6a3e3de9f5d58d0769c7bfe99836a536e63
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2024-04-11 12:32:26 -0600

    fix(atlassian): add missing attribute on test response

--- allauth/socialaccount/providers/atlassian/tests.py
+++ allauth/socialaccount/providers/atlassian/tests.py
@@ -13,6 +13,7 @@ class AtlassianTests(OAuth2TestsMixin, TestCase):
             "account_type": "atlassian",
             "account_id": "112233aa-bb11-cc22-33dd-445566abcabc",
             "email": "mia@example.com",
+            "email_verified": true,
             "name": "Mia Krystof",
             "picture": "https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/112233aa-bb11-cc22-33dd-445566abcabc/1234abcd-9876-54aa-33aa-1234dfsade9487ds",
             "account_status": "active",

03d9e53bd639685befbe34b473673619a77ffb05
Jonas Schalck <jonas.schalck@tftc.llc>
2024-04-16 16:42:29 +0200

    Initial commit

---
+++ allauth/socialaccount/providers/tiktok/provider.py
@@ -0,0 +1,45 @@
+from allauth.socialaccount.providers.base import ProviderAccount
+from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
+from allauth.socialaccount.providers.tiktok.views import TiktokOAuth2Adapter
+
+
+class TiktokAccount(ProviderAccount):
+
+    def get_username(self):
+        return self.account.extra_data.get("username")
+
+    def get_display_name(self):
+        return self.account.extra_data.get("display_name")
+
+    def get_profile_url(self):
+        return self.account.extra_data.get("profile_deep_link")
+
+    def get_avatar_url(self):
+        return self.account.extra_data.get("avatar_url")
+
+    def to_str(self):
+        username = self.get_username()
+        return username or super(TiktokAccount, self).to_str()
+
+
+class TiktokProvider(OAuth2Provider):
+    id = "tiktok"
+    name = "Tiktok"
+    account_class = TiktokAccount
+    oauth2_adapter_class = TiktokOAuth2Adapter
+
+    def extract_uid(self, data):
+        return str(data["open_id"])
+
+    def extract_common_fields(self, data):
+        return {
+            "username": data.get("username"),
+            "name": data.get("display_name"),
+            "email": data.get("email"),
+        }
+
+    def get_default_scope(self):
+        return ["user.info.basic", "user.info.profile"]
+
+
+provider_classes = [TiktokProvider]
---
+++ allauth/socialaccount/providers/tiktok/views.py
@@ -0,0 +1,45 @@
+from allauth.socialaccount.adapter import get_adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Error
+from allauth.socialaccount.providers.oauth2.views import (
+    OAuth2Adapter,
+    OAuth2CallbackView,
+    OAuth2LoginView,
+)
+
+
+class TiktokOAuth2Adapter(OAuth2Adapter):
+    provider_id = "tiktok"
+    access_token_url = "https://open.tiktokapis.com/v2/oauth/token/"
+    authorize_url = "https://www.tiktok.com/v2/auth/authorize/"
+    # https://developers.tiktok.com/doc/tiktok-api-v2-get-user-info/
+    profile_url = "https://open.tiktokapis.com/v2/user/info/"
+
+    def complete_login(self, request, app, token, **kwargs):
+        headers = {
+            "Authorization": "Bearer {}".format(token.token),
+            "Client-ID": app.client_id,
+        }
+        params = {"fields": "open_id,username,display_name,avatar_url,profile_deep_link"}
+        response = (
+            get_adapter().get_requests_session().get(self.profile_url, headers=headers, params=params)
+        )
+
+        data = response.json()
+        if response.status_code >= 400:
+            error = data.get("error", "")
+            message = data.get("message", "")
+            raise OAuth2Error("Tiktok API Error: %s (%s)" % (error, message))
+
+        try:
+            user_info = data.get("data", [])[0]
+        except IndexError:
+            raise OAuth2Error("Invalid data from Tiktok API: %s" % (data))
+
+        if "id" not in user_info:
+            raise OAuth2Error("Invalid data from Tiktok API: %s" % (user_info))
+
+        return self.get_provider().sociallogin_from_response(request, user_info)
+
+
+oauth2_login = OAuth2LoginView.adapter_view(TiktokOAuth2Adapter)
+oauth2_callback = OAuth2CallbackView.adapter_view(TiktokOAuth2Adapter)

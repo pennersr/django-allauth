624db320c7881a25842c5d609a78f854a973cbf6
Jonas Schalck <jonas.schalck@tftc.llc>
2024-04-24 16:03:43 +0200

    Remove typing

--- allauth/socialaccount/providers/tiktok/tests.py
+++ allauth/socialaccount/providers/tiktok/tests.py
@@ -1,5 +1,3 @@
-from typing import override
-
 from allauth.socialaccount.tests import OAuth2TestsMixin
 from allauth.tests import MockedResponse, TestCase
 
@@ -9,7 +7,6 @@ from .provider import TikTokProvider
 class TikTokTests(OAuth2TestsMixin, TestCase):
     provider_id = TikTokProvider.id
 
-    @override
     def get_mocked_response(self):
         return MockedResponse(
             200,

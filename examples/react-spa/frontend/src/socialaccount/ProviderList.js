3f663aaf6c7cfe824bf6398eb0564b93cfd672a4
Raymond Penners <raymond.penners@intenct.nl>
2024-04-26 11:23:03 +0200

    feat(headless): Don't install Google One Tap by default

--- examples/react-spa/frontend/src/socialaccount/ProviderList.js
+++ examples/react-spa/frontend/src/socialaccount/ProviderList.js
@@ -1,41 +1,26 @@
-import { useEffect } from 'react'
 import { useConfig } from '../auth'
-import { redirectToProvider, authenticateByToken } from '../lib/allauth'
+import { redirectToProvider } from '../lib/allauth'
 import Button from '../components/Button'
+import GoogleOneTap from './GoogleOneTap'
 
 export default function ProviderList (props) {
   const config = useConfig()
-  useEffect(() => {
-    const provider = config.data.socialaccount.providers.find(p => p.id === 'google')
-    if (provider && window.google) {
-      function handleCredentialResponse (token) {
-        authenticateByToken(provider.id, {
-          id_token: token.credential,
-          client_id: provider.client_id
-        }, props.process)
-      }
-
-      window.google.accounts.id.initialize({
-        client_id: provider.client_id,
-        callback: handleCredentialResponse
-      })
-      window.google.accounts.id.prompt()
-    }
-  }, [config, props.process])
-
   const providers = config.data.socialaccount.providers
   if (!providers.length) {
     return null
   }
   return (
-    <ul>
-      {providers.map(provider => {
-        return (
-          <li key={provider.id}>
-            <Button onClick={() => redirectToProvider(provider.id, props.callbackURL, props.process)}>{provider.name}</Button>
-          </li>
-        )
-      })}
-    </ul>
+    <>
+      <GoogleOneTap process={props.process} />
+      <ul>
+        {providers.map(provider => {
+          return (
+            <li key={provider.id}>
+              <Button onClick={() => redirectToProvider(provider.id, props.callbackURL, props.process)}>{provider.name}</Button>
+            </li>
+          )
+        })}
+      </ul>
+    </>
   )
 }

a4177262a5d9f75db29244fe363f7ddc5a4657b7
Radek Šenfeld <rush@logic.cz>
2024-04-28 13:35:55 +0200

    fix(utils): Cannot serialize BinaryField wihout a value
    * Cannot serialize BinaryField wihout a value
    
    * Update allauth/utils.py
    
    Co-authored-by: Raymond Penners <raymond.penners@intenct.nl>
    
    ---------
    
    Co-authored-by: Raymond Penners <raymond.penners@intenct.nl>

--- allauth/utils.py
+++ allauth/utils.py
@@ -172,7 +172,8 @@ def serialize_instance(instance):
         try:
             field = instance._meta.get_field(k)
             if isinstance(field, BinaryField):
-                v = force_str(base64.b64encode(v))
+                if v is not None:
+                    v = force_str(base64.b64encode(v))
             elif isinstance(field, FileField):
                 if v and not isinstance(v, str):
                     v = {

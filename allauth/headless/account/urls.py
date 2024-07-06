b4228c87d3d44fc23b6290091329767ec08a3070
Raymond Penners <raymond.penners@intenct.nl>
2024-07-06 12:58:41 +0200

    refactor(headless): Rearranged build_urlpatterns()

--- allauth/headless/account/urls.py
+++ allauth/headless/account/urls.py
@@ -5,89 +5,81 @@ from allauth.headless.account import views
 
 
 def build_urlpatterns(client):
-    return [
+    account_patterns = [
         path(
-            "auth/",
+            "password/change",
+            views.ChangePasswordView.as_api_view(client=client),
+            name="change_password",
+        ),
+        path(
+            "email",
+            views.ManageEmailView.as_api_view(client=client),
+            name="manage_email",
+        ),
+    ]
+    auth_patterns = [
+        path(
+            "session",
+            views.SessionView.as_api_view(client=client),
+            name="current_session",
+        ),
+        path(
+            "password/",
             include(
                 [
                     path(
-                        "session",
-                        views.SessionView.as_api_view(client=client),
-                        name="current_session",
+                        "request",
+                        views.RequestPasswordResetView.as_api_view(client=client),
+                        name="request_password_reset",
                     ),
                     path(
-                        "password/",
-                        include(
-                            [
-                                path(
-                                    "request",
-                                    views.RequestPasswordResetView.as_api_view(
-                                        client=client
-                                    ),
-                                    name="request_password_reset",
-                                ),
-                                path(
-                                    "reset",
-                                    views.ResetPasswordView.as_api_view(client=client),
-                                    name="reset_password",
-                                ),
-                            ]
-                        ),
-                    ),
-                    path(
-                        "login",
-                        views.LoginView.as_api_view(client=client),
-                        name="login",
-                    ),
-                    path(
-                        "reauthenticate",
-                        views.ReauthenticateView.as_api_view(client=client),
-                        name="reauthenticate",
-                    ),
-                    path(
-                        "signup",
-                        views.SignupView.as_api_view(client=client),
-                        name="signup",
-                    ),
-                    path(
-                        "email/verify",
-                        views.VerifyEmailView.as_api_view(client=client),
-                        name="verify_email",
+                        "reset",
+                        views.ResetPasswordView.as_api_view(client=client),
+                        name="reset_password",
                     ),
                 ]
-                + (
-                    [
-                        path(
-                            "code/request",
-                            views.RequestLoginCodeView.as_api_view(client=client),
-                            name="request_login_code",
-                        ),
-                        path(
-                            "code/confirm",
-                            views.ConfirmLoginCodeView.as_api_view(client=client),
-                            name="confirm_login_code",
-                        ),
-                    ]
-                    if account_settings.LOGIN_BY_CODE_ENABLED
-                    else []
-                )
             ),
         ),
         path(
+            "login",
+            views.LoginView.as_api_view(client=client),
+            name="login",
+        ),
+        path(
+            "reauthenticate",
+            views.ReauthenticateView.as_api_view(client=client),
+            name="reauthenticate",
+        ),
+        path(
+            "signup",
+            views.SignupView.as_api_view(client=client),
+            name="signup",
+        ),
+        path(
+            "email/verify",
+            views.VerifyEmailView.as_api_view(client=client),
+            name="verify_email",
+        ),
+    ]
+    if account_settings.LOGIN_BY_CODE_ENABLED:
+        auth_patterns.extend(
+            [
+                path(
+                    "code/request",
+                    views.RequestLoginCodeView.as_api_view(client=client),
+                    name="request_login_code",
+                ),
+                path(
+                    "code/confirm",
+                    views.ConfirmLoginCodeView.as_api_view(client=client),
+                    name="confirm_login_code",
+                ),
+            ]
+        )
+    return [
+        path("auth/", include(auth_patterns)),
+        path(
             "account/",
-            include(
-                [
-                    path(
-                        "password/change",
-                        views.ChangePasswordView.as_api_view(client=client),
-                        name="change_password",
-                    ),
-                    path(
-                        "email",
-                        views.ManageEmailView.as_api_view(client=client),
-                        name="manage_email",
-                    ),
-                ]
-            ),
+            include(account_patterns),
         ),
     ]

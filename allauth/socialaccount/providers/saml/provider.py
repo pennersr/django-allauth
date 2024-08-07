28538ce790d56b92748c5d071d34578a231899c9
Allen, Timothy <tallen@wharton.upenn.edu>
2024-08-07 15:07:37 -0400

    Add email as default to email_verified.

--- allauth/socialaccount/providers/saml/provider.py
+++ allauth/socialaccount/providers/saml/provider.py
@@ -24,6 +24,7 @@ class SAMLProvider(Provider):
             "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress",
         ],
         "email_verified": [
+            "urn:oid:0.9.2342.19200300.100.1.3",
             "http://schemas.auth0.com/email_verified",
         ],
         "first_name": [

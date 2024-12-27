daf07aa26c7625d6f7a574ee8eb36e906cdead6f
Raymond Penners <raymond.penners@intenct.nl>
2024-12-27 17:40:11 +0100

    fix(headless/spec): login_not_required

--- allauth/headless/spec/views.py
+++ allauth/headless/spec/views.py
@@ -1,12 +1,15 @@
 import json
 
 from django.http import HttpResponse
+from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView, View
 
+from allauth.account.internal.decorators import login_not_required
 from allauth.headless import app_settings
 from allauth.headless.spec.internal.schema import get_schema
 
 
+@method_decorator(login_not_required, name="dispatch")
 class OpenAPIYAMLView(View):
     def get(self, request):
         import yaml
@@ -20,6 +23,7 @@ class OpenAPIYAMLView(View):
         )
 
 
+@method_decorator(login_not_required, name="dispatch")
 class OpenAPIJSONView(View):
     def get(self, request):
         spec = get_schema()
@@ -31,6 +35,7 @@ class OpenAPIJSONView(View):
         )
 
 
+@method_decorator(login_not_required, name="dispatch")
 class OpenAPIHTMLView(TemplateView):
     def get_template_names(self):
         return [app_settings.SPECIFICATION_TEMPLATE_NAME]

2bd5c3a5cc87d9d3df99a76fc2212062635d82d3
Andre Borie <hi@rjevski.io>
2025-01-12 11:26:44 +0000

    fix(openid_connect): gracefully handle requests with invalid provider IDs
    This PR fixes an issue where external requests to OIDC endpoints quoting an unknown provider ID would end up with an unhandled `SocialApp.DoesNotExist` exception.
    
    As this issue is triggerable from the outside via an invalid URL, I think it's fair to swallow the exception and respond with a generic 404 error.
    
    I have added a unit test to confirm the behavior.
    
    # Submitting Pull Requests
    
    ## General
    
     - [x] Make sure you use [semantic commit messages](https://seesparkbox.com/foundry/semantic_commit_messages).
           Examples: `"fix(google): Fixed foobar bug"`, `"feat(accounts): Added foobar feature"`.
     - [x] All Python code must formatted using Black, and clean from pep8 and isort issues.
     - [ ] JavaScript code should adhere to [StandardJS](https://standardjs.com).
     - [ ] If your changes are significant, please update `ChangeLog.rst`.
     - [ ] If your change is substantial, feel free to add yourself to `AUTHORS`.
    
     ## Provider Specifics
    
     In case you add a new provider:
    
    - [x] Make sure unit tests are available.
    - [ ] Add an entry of your provider in `test_settings.py::INSTALLED_APPS` and `docs/installation.rst::INSTALLED_APPS`.
    - [ ] Add documentation to `docs/providers/<provider name>.rst` and `docs/providers/index.rst` Provider Specifics toctree.
    - [ ] Add an entry to the list of supported providers over at `docs/overview.rst`.
    
    Reviewed-on: https://codeberg.org/allauth/django-allauth/pulls/4220
    Co-authored-by: Andre Borie <hi@rjevski.io>
    Co-committed-by: Andre Borie <hi@rjevski.io>

--- allauth/socialaccount/providers/openid_connect/views.py
+++ allauth/socialaccount/providers/openid_connect/views.py
@@ -1,8 +1,9 @@
+from django.http import Http404
 from django.urls import reverse
 
 from allauth.account.internal.decorators import login_not_required
 from allauth.socialaccount.adapter import get_adapter
-from allauth.socialaccount.models import SocialToken
+from allauth.socialaccount.models import SocialApp, SocialToken
 from allauth.socialaccount.providers.oauth2.views import (
     OAuth2Adapter,
     OAuth2CallbackView,
@@ -66,15 +67,21 @@ class OpenIDConnectOAuth2Adapter(OAuth2Adapter):
 
 @login_not_required
 def login(request, provider_id):
-    view = OAuth2LoginView.adapter_view(
-        OpenIDConnectOAuth2Adapter(request, provider_id)
-    )
-    return view(request)
+    try:
+        view = OAuth2LoginView.adapter_view(
+            OpenIDConnectOAuth2Adapter(request, provider_id)
+        )
+        return view(request)
+    except SocialApp.DoesNotExist:
+        raise Http404
 
 
 @login_not_required
 def callback(request, provider_id):
-    view = OAuth2CallbackView.adapter_view(
-        OpenIDConnectOAuth2Adapter(request, provider_id)
-    )
-    return view(request)
+    try:
+        view = OAuth2CallbackView.adapter_view(
+            OpenIDConnectOAuth2Adapter(request, provider_id)
+        )
+        return view(request)
+    except SocialApp.DoesNotExist:
+        raise Http404
--- allauth/socialaccount/tests/__init__.py
+++ allauth/socialaccount/tests/__init__.py
@@ -446,3 +446,41 @@ class OpenIDConnectTests(OAuth2TestsMixin):
         self.assertRedirects(resp, "/accounts/profile/", fetch_redirect_response=False)
         sa = SocialAccount.objects.get(provider=self.app.provider_id)
         self.assertDictEqual(sa.extra_data, self.extra_data)
+
+    def test_404_on_unknown_provider_id(self):
+        """
+        Make sure that OIDC endpoints hit with an invalid provider_id
+        not corresponding to any configured social "apps" returns a 404
+        instead of an unhandled SocialApp.DoesNotExist.
+        """
+
+        # we can't use self.provider.get_login_url as we intentionally
+        # do not want to use the configured provider's ID, so let's inline
+        # OpenIDConnectProvider.get_login_url
+        login_url = reverse(
+            self.app.provider + "_login",
+            kwargs={
+                # intentionally invalidate the ID
+                "provider_id": self.app.provider_id
+                + "-invalid"
+            },
+        )
+
+        resp = self.client.post(login_url)
+
+        self.assertEqual(resp.status_code, 404)
+
+        # same with the callback endpoint - inlining OpenIDConnectProvider.get_callback_url
+        callback_url = reverse(
+            self.app.provider + "_callback",
+            kwargs={
+                # intentionally invalidate the ID
+                "provider_id": self.app.provider_id
+                + "-invalid"
+            },
+        )
+
+        # note: callback is a GET endpoint
+        resp = self.client.get(callback_url)
+
+        self.assertEqual(resp.status_code, 404)

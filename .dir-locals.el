a972906b14a4afa635b8f53eda9c12ce6b224532
Raymond Penners <raymond.penners@intenct.nl>
2025-01-24 15:15:57 +0100

    chore: Change formatter in .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,2 @@
 ;;; This file contains project-specific emacs configuration
-((nil . ((python-sort-imports-on-save t)
-(format-all-formatters
- ("JavaScript" standard)
- ("Python" black)))))
+((python-mode . ((apheleia-formatter . (black isort)))))

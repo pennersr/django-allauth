bf238b33f291d5729fd259b66c88239cf8cde44a
Fadel Mochammad <fadelnavi@gmail.com>
2025-06-11 08:47:05 +0700

    Add inline comment to manage.py

--- manage.py
+++ manage.py
@@ -1,3 +1,17 @@
+```python
+# This file is the main entry point for running Django management commands.
+#!/usr/bin/env python
+import os
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.regular.settings")
+from django.core import management  # noqa: E402
+
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
+...
+``
 #!/usr/bin/env python
 import os
 

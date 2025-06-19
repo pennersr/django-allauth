5208c8c70af9d65920906b64dee5ce867f646f67
Raymond Penners <raymond.penners@intenct.nl>
2025-06-19 10:45:46 +0200

    feat(mfa): allow adding email when verifying by code

--- allauth/mfa/base/tests/test_views.py
+++ allauth/mfa/base/tests/test_views.py
@@ -1,3 +1,4 @@
+from http import HTTPStatus
 from unittest.mock import ANY
 
 from django.urls import reverse
@@ -11,7 +12,7 @@ from allauth.mfa.models import Authenticator
 
 def test_reauthentication(auth_client, user_with_recovery_codes):
     resp = auth_client.get(reverse("mfa_view_recovery_codes"))
-    assert resp.status_code == 302
+    assert resp.status_code == HTTPStatus.FOUND
     assert resp["location"].startswith(reverse("account_reauthenticate"))
     resp = auth_client.get(reverse("mfa_reauthenticate"))
     assertTemplateUsed(resp, "mfa/reauthenticate.html")
@@ -20,9 +21,9 @@ def test_reauthentication(auth_client, user_with_recovery_codes):
     )
     unused_code = authenticator.wrap().get_unused_codes()[0]
     resp = auth_client.post(reverse("mfa_reauthenticate"), data={"code": unused_code})
-    assert resp.status_code == 302
+    assert resp.status_code == HTTPStatus.FOUND
     resp = auth_client.get(reverse("mfa_view_recovery_codes"))
-    assert resp.status_code == 200
+    assert resp.status_code == HTTPStatus.OK
     assertTemplateUsed(resp, "mfa/recovery_codes/index.html")
     methods = auth_client.session[AUTHENTICATION_METHODS_SESSION_KEY]
     assert methods[-1] == {
@@ -44,7 +45,7 @@ def test_reauthentication(auth_client, user_with_recovery_codes):
 )
 def test_login_required_views(client, url_name):
     resp = client.get(reverse(url_name))
-    assert resp.status_code == 302
+    assert resp.status_code == HTTPStatus.FOUND
     assert resp["location"].startswith(reverse("account_login"))
 
 
@@ -53,14 +54,31 @@ def test_index(auth_client, user_with_totp):
     assert "authenticators" in resp.context
 
 
-def test_add_email_not_allowed(auth_client, user_with_totp):
+def test_add_email_not_allowed(
+    auth_client,
+    user_with_totp,
+):
     resp = auth_client.post(
         reverse("account_email"),
         {"action_add": "", "email": "change-to@this.org"},
     )
-    assert resp.status_code == 200
+    assert resp.status_code == HTTPStatus.OK
     assert resp.context["form"].errors == {
         "email": [
             "You cannot add an email address to an account protected by two-factor authentication."
         ]
     }
+
+
+def test_add_email_allowed_when_verification_by_code(
+    auth_client,
+    user_with_totp,
+    settings,
+):
+    settings.ACCOUNT_EMAIL_VERIFICATION_BY_CODE_ENABLED = True
+    resp = auth_client.post(
+        reverse("account_email"),
+        {"action_add": "", "email": "change-to@this.org"},
+    )
+    assert resp.status_code == HTTPStatus.FOUND
+    assert resp["location"] == reverse("account_email_verification_sent")
--- allauth/mfa/signals.py
+++ allauth/mfa/signals.py
@@ -1,5 +1,6 @@
 from django.dispatch import Signal
 
+from allauth.account import app_settings as account_settings
 from allauth.mfa.adapter import get_adapter
 from allauth.mfa.utils import is_mfa_enabled
 
@@ -18,6 +19,8 @@ authenticator_reset = Signal()
 
 
 def on_add_email(sender, email, user, **kwargs):
+    if account_settings.EMAIL_VERIFICATION_BY_CODE_ENABLED:
+        return
     if is_mfa_enabled(user):
         adapter = get_adapter()
         raise adapter.validation_error("add_email_blocked")

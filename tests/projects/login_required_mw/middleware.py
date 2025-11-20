44de7d40c94ccc81a020763bbeb7c7697771257b
Raymond Penners <raymond.penners@intenct.nl>
2025-11-20 13:34:12 +0100

    tests: ninja 1.5.0 compatibility

--- tests/projects/login_required_mw/middleware.py
+++ tests/projects/login_required_mw/middleware.py
@@ -4,6 +4,6 @@ from django.contrib.auth.middleware import LoginRequiredMiddleware
 class CustomLoginRequiredMiddleware(LoginRequiredMiddleware):
     def handle_no_permission(self, request, view_func):
         # Quick & dirty workaround for: https://github.com/vitalik/django-ninja/issues/1461
-        if "ninja.operation.PathView" in repr(view_func):
+        if "/ninja/" in request.path:
             return None
         return super().handle_no_permission(request, view_func)

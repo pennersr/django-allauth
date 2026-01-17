592be5bb073342a7666a775df25f0466415378c8
Raymond Penners <raymond.penners@intenct.nl>
2026-01-17 14:19:20 +0100

    feat(usersessions/admin): search by user, ip

--- allauth/usersessions/admin.py
+++ allauth/usersessions/admin.py
@@ -7,3 +7,8 @@ from allauth.usersessions.models import UserSession
 class UserSessionAdmin(admin.ModelAdmin):
     raw_id_fields = ("user",)
     list_display = ("user", "created_at", "last_seen_at", "ip", "user_agent")
+    search_fields = [
+        "user__pk",
+        "user__username",
+        "ip",
+    ]
